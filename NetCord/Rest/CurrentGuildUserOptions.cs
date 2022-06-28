44406484be6ca18bbffc1043f460ef402efbad3b
KubaZ2 <jazloty@o2.pl>
2022-06-28 12:27:09 +0200

    Fixed name of `CurrentGuildUserOptions`

---
+++ NetCord/Rest/CurrentGuildUserOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NetCord.Rest;
+
+public class CurrentGuildUserOptions
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("nick")]
+    public string? Nickname { get; set; }
+}
