cb671824842c1ea92516c296b495358dc53bbd6b
KubaZ2 <jazloty@o2.pl>
2022-08-11 11:56:52 +0200

    Update `NetCord.Gateway.GatewayVersion`

--- NetCord/Gateway/GatewayVersion.cs
+++ NetCord/Gateway/GatewayVersion.cs
@@ -3,13 +3,16 @@
 public enum GatewayVersion
 {
     [Obsolete("Discontinued")]
+    V3 = 3,
+    [Obsolete("Discontinued")]
     V4 = 4,
     [Obsolete("Discontinued")]
     V5 = 5,
     [Obsolete("Deprecated")]
     V6 = 6,
-    [Obsolete("Doesn't look like anything to me")]
+    [Obsolete("Deprecated")]
     V7 = 7,
+    [Obsolete("Deprecated")]
     V8 = 8,
     V9 = 9,
     V10 = 10,
