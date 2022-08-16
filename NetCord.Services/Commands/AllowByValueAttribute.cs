bfbef64b12af3b46ccab14ae04e48d82f00831f8
KubaZ2 <jazloty@o2.pl>
2022-08-16 14:59:05 +0200

    Ignore `AllowByValueAttribute` in enum type reader in `InteractionService`

--- NetCord.Services/Commands/AllowByValueAttribute.cs
+++ NetCord.Services/Commands/AllowByValueAttribute.cs
@@ -1,4 +1,4 @@
-﻿namespace NetCord.Services;
+﻿namespace NetCord.Services.Commands;
 
 [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Parameter)]
 public class AllowByValueAttribute : Attribute
--- NetCord.Services/Interactions/TypeReaders/EnumTypeReader.cs
+++ NetCord.Services/Interactions/TypeReaders/EnumTypeReader.cs
@@ -1,23 +1,12 @@
-﻿using System.Reflection;
-
-namespace NetCord.Services.Interactions.TypeReaders;
+﻿namespace NetCord.Services.Interactions.TypeReaders;
 
 public class EnumTypeReader<TContext> : InteractionTypeReader<TContext> where TContext : InteractionContext
 {
     public override Task<object?> ReadAsync(ReadOnlyMemory<char> input, TContext context, InteractionParameter<TContext> parameter, InteractionServiceOptions<TContext> options)
     {
-        var span = input.Span;
         var type = parameter.Type;
-        if (parameter.Attributes.ContainsKey(typeof(AllowByValueAttribute)) || type.IsDefined(typeof(AllowByValueAttribute))) // by value or by name
-        {
-            if (Enum.TryParse(type, span, options.IgnoreCase, out var value) && Enum.IsDefined(type, value!))
-                return Task.FromResult(value);
-        }
-        else // by name
-        {
-            if (((uint)span[0] - '0') > 9 && Enum.TryParse(type, span, options.IgnoreCase, out var value))
-                return Task.FromResult(value);
-        }
+        if (Enum.TryParse(type, input.Span, options.IgnoreCase, out var value) && Enum.IsDefined(type, value!))
+            return Task.FromResult(value);
 
         throw new FormatException($"Invalid {type.Name}.");
     }
