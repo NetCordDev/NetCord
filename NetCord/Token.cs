dca641a416bbd384697abfccf92387cef6169404
KubaZ2 <jazloty@o2.pl>
2022-10-23 23:58:48 +0200

    Remove `goto` usage

--- NetCord/Token.cs
+++ NetCord/Token.cs
@@ -14,25 +14,25 @@ public class Token
     }
 
     public TokenType Type { get; }
-    public string RawToken { get; }
 
-    public string ToHttpHeader() => Type == TokenType.Bot ? $"Bot {RawToken}" : RawToken;
+    public string RawToken { get; }
 
     public ulong Id
     {
         get
         {
-            var index = RawToken.IndexOf('.');
-            if (index == -1)
-                goto Invalid;
+            int index = RawToken.IndexOf('.');
+            if (index != -1)
+            {
+                var idBase64 = RawToken[..index];
+                var idConverted = Convert.FromBase64String(idBase64.PadRight((idBase64.Length + 3) / 4 * 4, '='));
+                if (Utf8Parser.TryParse(idConverted, out ulong id, out int bytesConsumed) && idConverted.Length == bytesConsumed)
+                    return id;
+            }
 
-            var idBase64 = RawToken[..index];
-            var idConverted = Convert.FromBase64String(idBase64.PadRight((idBase64.Length + 3) / 4 * 4, '='));
-            if (Utf8Parser.TryParse(idConverted, out ulong id, out int bytesConsumed) && idConverted.Length == bytesConsumed)
-                return id;
-
-            Invalid:
             throw new InvalidOperationException("Invalid token provided.");
         }
     }
+
+    public string ToHttpHeader() => Type == TokenType.Bot ? $"Bot {RawToken}" : RawToken;
 }
