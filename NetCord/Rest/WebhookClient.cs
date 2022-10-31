1c4bac43e4c96b79cee7ecb605df8bd7a5942457
KubaZ2 <jazloty@o2.pl>
2022-10-31 19:57:15 +0100

    Fix `WebhookClient`

--- NetCord/Rest/WebhookClient.cs
+++ NetCord/Rest/WebhookClient.cs
@@ -1,20 +1,24 @@
 ﻿namespace NetCord.Rest;
 
-public class WebhookClient
+public class WebhookClient : IDisposable
 {
     public ulong Id { get; }
 
     private readonly string _token;
     private readonly RestClient _client;
+    private bool _dispose;
 
     public WebhookClient(ulong webhookId, string webhookToken, WebhookClientConfig? config = null)
     {
         Id = webhookId;
         _token = webhookToken;
-        if (config == null)
+        if (config == null || config.Client == null)
+        {
+            _dispose = true;
             _client = new();
+        }
         else
-            _client = config.Client ?? new();
+            _client = config.Client;
     }
 
     public Task<Webhook> GetAsync(RequestProperties? properties = null) => _client.GetWebhookWithTokenAsync(Id, _token, properties);
@@ -24,4 +28,10 @@ public class WebhookClient
     public Task<WebhookMessage> GetMessageAsync(ulong messageId, RequestProperties? properties = null) => _client.GetWebhookMessageAsync(Id, _token, messageId, properties);
     public Task<WebhookMessage> ModifyMessageAsync(ulong messageId, Action<MessageOptions> action, ulong? threadId = null, RequestProperties? properties = null) => _client.ModifyWebhookMessageAsync(Id, _token, messageId, action, threadId, properties);
     public Task DeleteMessageAsync(ulong messageId, ulong? threadId = null, RequestProperties? properties = null) => _client.DeleteWebhookMessageAsync(Id, _token, messageId, threadId, properties);
+
+    public void Dispose()
+    {
+        if (_dispose)
+            _client.Dispose();
+    }
 }
