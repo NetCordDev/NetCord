755eecc4950b242fbc3146440ecc529410259138
KubaZ2 <jazloty@o2.pl>
2022-11-15 16:05:13 +0100

    Optimize `JsonContent`

---
+++ NetCord/Rest/HttpContents/JsonContent.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace NetCord.Rest;
+
+internal class JsonContent<T> : HttpContent
+{
+    private readonly T _objToSerialize;
+    private readonly JsonTypeInfo<T> _jsonTypeInfo;
+
+    public JsonContent(T objToSerialize, JsonTypeInfo<T> jsonTypeInfo)
+    {
+        _objToSerialize = objToSerialize;
+        _jsonTypeInfo = jsonTypeInfo;
+        Headers.ContentType = new("application/json");
+    }
+
+    protected override Task SerializeToStreamAsync(Stream stream, TransportContext? context) => JsonSerializer.SerializeAsync(stream, _objToSerialize, _jsonTypeInfo);
+
+    protected override bool TryComputeLength(out long length)
+    {
+        length = 0;
+        return false;
+    }
+}
