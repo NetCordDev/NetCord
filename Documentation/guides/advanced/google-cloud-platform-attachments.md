4a29a7fac36ab2cf80e35058e54f9f4254ca2175
KubaZ2 <jazloty@o2.pl>
2022-12-24 20:57:13 +0100

    Add Google Cloud Platform attachment docs

---
+++ Documentation/guides/advanced/google-cloud-platform-attachments.md
@@ -0,0 +1,14 @@
+# Google Cloud Platform Attachments
+
+> [!WARNING]
+> Discord does not offer any stability guarantees for this feature so it can break without notice.
+
+Discord supports uploading attachments directly on their Google Cloud Platform. You can then send the same file multiple times without sending its content multiple times.
+
+## Uploading File to Google Cloud Platform
+
+[!code-cs[Program.cs](google-cloud-platform-attachments/uploading/Program.cs)]
+
+## Sending Google Cloud Platform Attachment
+
+[!code-cs[Program.cs](google-cloud-platform-attachments/sending/Program.cs)]
---
+++ Documentation/guides/advanced/google-cloud-platform-attachments/uploading/Program.cs
@@ -0,0 +1,25 @@
+using NetCord;
+using NetCord.Rest;
+
+using RestClient client = new(new Token(TokenType.Bot, "Token from Discord Developer Portal"));
+
+ulong channelId = 857933275112800266;
+var bucketName = "file.txt";
+
+// Creating Google Cloud Platform Storage Bucket
+var buckets = await client.CreateGoogleCloudPlatformStorageBucketAsync(channelId, new GoogleCloudPlatformStorageBucketProperties[]
+{
+    new(bucketName),
+});
+var bucket = buckets.First();
+
+HttpContent fileContent = new StringContent("File content");
+using (HttpClient httpClient = new())
+{
+    // Uploading file content
+    var response = await httpClient.PutAsync(bucket.UploadUrl, fileContent);
+    response.EnsureSuccessStatusCode();
+}
+
+var uploadFileName = bucket.UploadFileName;
+Console.WriteLine(uploadFileName);
