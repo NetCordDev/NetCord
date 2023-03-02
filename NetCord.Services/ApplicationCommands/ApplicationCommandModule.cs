8950beed608d8449dfe929bb3b02e27a52e555ca
KubaZ2 <jazloty@o2.pl>
2023-03-02 17:26:50 +0100

    Add interaction methods to `ApplicationCommandModule<TContext>` and `InteractionModule<TContext>`

--- NetCord.Services/ApplicationCommands/ApplicationCommandModule.cs
+++ NetCord.Services/ApplicationCommands/ApplicationCommandModule.cs
@@ -5,4 +5,18 @@ namespace NetCord.Services.ApplicationCommands;
 public class ApplicationCommandModule<TContext> : BaseApplicationCommandModule<TContext> where TContext : IApplicationCommandContext
 {
     public Task RespondAsync(InteractionCallback callback, RequestProperties? properties = null) => Context.Interaction.SendResponseAsync(callback, properties);
+    
+    public Task<RestMessage> GetResponseAsync(RequestProperties? properties = null) => Context.Interaction.GetResponseAsync(properties);
+    
+    public Task<RestMessage> ModifyResponseAsync(Action<MessageOptions> action, RequestProperties? properties = null) => Context.Interaction.ModifyResponseAsync(action, properties);
+    
+    public Task DeleteResponseAsync(RequestProperties? properties = null) => Context.Interaction.DeleteResponseAsync(properties);
+    
+    public Task<RestMessage> FollowupAsync(InteractionMessageProperties message, RequestProperties? properties = null) => Context.Interaction.SendFollowupMessageAsync(message, properties);
+    
+    public Task<RestMessage> GetFollowupAsync(ulong messageId, RequestProperties? properties = null) => Context.Interaction.GetFollowupMessageAsync(messageId, properties);
+    
+    public Task<RestMessage> ModifyFollowupAsync(ulong messageId, Action<MessageOptions> action, RequestProperties? properties = null) => Context.Interaction.ModifyFollowupMessageAsync(messageId, action, properties);
+    
+    public Task DeleteFollowupAsync(ulong messageId, RequestProperties? properties = null) => Context.Interaction.DeleteFollowupMessageAsync(messageId, properties);
 }
