efbc582b1c6e78a3792867a71b90cdcf56cec122
KubaZ2 <jazloty@o2.pl>
2023-04-21 16:09:14 +0200

    Fix `CollectionsUtils` for `ImmutableArray<T>`

--- NetCord/Utils/CollectionUtils.cs
+++ NetCord/Utils/CollectionUtils.cs
@@ -4,6 +4,7 @@ namespace NetCord;
 
 internal static class CollectionsUtils
 {
+    #region IEnumerable<out T>
     public static Dictionary<TKey, TSource> ToDictionaryOrEmpty<TSource, TKey>(this IEnumerable<TSource>? source, Func<TSource, TKey> keySelector) where TKey : notnull
     {
         if (source == null)
@@ -66,6 +67,59 @@ internal static class CollectionsUtils
     {
         return CreateImmutableDictionary<TKey, TElement>().AddRange(source.Select(p => new KeyValuePair<TKey, TElement>(p.Key, elementSelector(p.Value))));
     }
+    #endregion
+
+    #region ImmutableArray<T>
+    public static Dictionary<TKey, TSource> ToDictionaryOrEmpty<TSource, TKey>(this ImmutableArray<TSource> source, Func<TSource, TKey> keySelector) where TKey : notnull
+    {
+        if (source.IsDefaultOrEmpty)
+            return new();
+        else
+            return source.ToDictionary(keySelector);
+    }
+
+    public static Dictionary<TKey, TElement> ToDictionaryOrEmpty<TSource, TKey, TElement>(this ImmutableArray<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector) where TKey : notnull
+    {
+        if (source.IsDefaultOrEmpty)
+            return new();
+        else
+            return source.ToDictionary(keySelector, elementSelector);
+    }
+
+    public static IEnumerable<TResult> SelectOrEmpty<TSource, TResult>(this ImmutableArray<TSource> source, Func<TSource, TResult> selector)
+    {
+        if (source.IsDefaultOrEmpty)
+            return Enumerable.Empty<TResult>();
+        else
+            return source.Select(selector);
+    }
+
+    public static ImmutableDictionary<TKey, TSource> ToImmutableDictionaryOrEmpty<TSource, TKey>(this ImmutableArray<TSource> source, Func<TSource, TKey> keySelector) where TKey : notnull
+    {
+        if (source.IsDefaultOrEmpty)
+            return CreateImmutableDictionary<TKey, TSource>();
+        else
+            return source.ToImmutableDictionary(keySelector);
+    }
+
+    public static ImmutableDictionary<TKey, TElement> ToImmutableDictionaryOrEmpty<TSource, TKey, TElement>(this ImmutableArray<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector) where TKey : notnull
+    {
+        if (source.IsDefaultOrEmpty)
+            return CreateImmutableDictionary<TKey, TElement>();
+        else
+            return source.ToImmutableDictionary(keySelector, elementSelector);
+    }
+
+    public static ImmutableDictionary<TKey, TSource> ToImmutableDictionary<TSource, TKey>(this ImmutableArray<TSource> source, Func<TSource, TKey> keySelector) where TKey : notnull
+    {
+        return CreateImmutableDictionary<TKey, TSource>().AddRange(source.Select(s => new KeyValuePair<TKey, TSource>(keySelector(s), s)));
+    }
+
+    public static ImmutableDictionary<TKey, TElement> ToImmutableDictionary<TSource, TKey, TElement>(this ImmutableArray<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector) where TKey : notnull
+    {
+        return CreateImmutableDictionary<TKey, TElement>().AddRange(source.Select(s => new KeyValuePair<TKey, TElement>(keySelector(s), elementSelector(s))));
+    }
+    #endregion
 
     public static ImmutableDictionary<TKey, TElement> CreateImmutableDictionary<TKey, TElement>() where TKey : notnull => ImmutableDictionary<TKey, TElement>.Empty.WithComparers(null, new ReferenceEqualityComparer<TElement>());
 }
