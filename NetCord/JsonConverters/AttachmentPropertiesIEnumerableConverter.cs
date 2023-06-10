bc0461a98e15dc582650a6a6f843d02394f66641
KubaZ2 <jazloty@o2.pl>
2023-06-10 10:35:06 +0200

    Refactor `AttachmentPropertiesIEnumerableConverter`

--- NetCord/JsonConverters/AttachmentPropertiesIEnumerableConverter.cs
+++ NetCord/JsonConverters/AttachmentPropertiesIEnumerableConverter.cs
@@ -13,22 +13,32 @@ internal class AttachmentPropertiesIEnumerableConverter : JsonConverter<IEnumera
     private static readonly JsonEncodedText _uploadedFileName = JsonEncodedText.Encode("uploaded_filename");
 
     public override IEnumerable<AttachmentProperties>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => throw new NotImplementedException();
+
     public override void Write(Utf8JsonWriter writer, IEnumerable<AttachmentProperties> value, JsonSerializerOptions options)
     {
         writer.WriteStartArray();
+
         int i = 0;
         foreach (var attachment in value)
         {
             writer.WriteStartObject();
+
             writer.WriteNumber(_id, i);
+
             writer.WriteString(_fileName, attachment.FileName);
-            if (attachment.Description != null)
-                writer.WriteString(_description, attachment.Description);
+
+            var description = attachment.Description;
+            if (description != null)
+                writer.WriteString(_description, description);
+
             if (attachment is GoogleCloudPlatformAttachmentProperties googleCloudPlatformAttachment)
                 writer.WriteString(_uploadedFileName, googleCloudPlatformAttachment.UploadedFileName);
+
             writer.WriteEndObject();
+
             i++;
         }
+
         writer.WriteEndArray();
     }
 }
