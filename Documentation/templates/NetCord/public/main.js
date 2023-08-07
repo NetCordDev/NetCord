3d2822055b582c2aae7bf027bce9bed241ee0081
KubaZ2 <jazloty@o2.pl>
2023-08-07 17:26:02 +0200

    Update docfx, add load animation to docs, add generation of docs from source and add discord guild button to docs

---
+++ Documentation/templates/NetCord/public/main.js
@@ -0,0 +1,14 @@
+export default {
+    iconLinks: [
+        {
+            href: 'https://github.com/KubaZ2/NetCord',
+            title: 'GitHub',
+            icon: 'github'
+        },
+        {
+            href: 'https://discord.gg/meaSHTGyUH',
+            title: 'Discord',
+            icon: 'discord'
+        }
+    ]
+}
