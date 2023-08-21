10949df5577e99acc6e5d8b2aaf3fc69e5e7f5d8
KubaZ2 <jazloty@o2.pl>
2023-08-21 15:23:54 +0200

    Add ligatures toggle

--- Documentation/templates/NetCord/public/main.js
+++ Documentation/templates/NetCord/public/main.js
@@ -1,14 +1,37 @@
-export default {
-    iconLinks: [
-        {
-            href: 'https://github.com/KubaZ2/NetCord',
-            title: 'GitHub',
-            icon: 'github'
-        },
-        {
-            href: 'https://discord.gg/meaSHTGyUH',
-            title: 'Discord',
-            icon: 'discord'
-        }
-    ]
+let ligatures = localStorage.getItem("ligatures");
+
+if (!ligatures)
+    localStorage.setItem("ligatures", ligatures = "0");
+
+document.documentElement.setAttribute("ligatures", ligatures);
+
+function changeLigatures() {
+    const ligatures = localStorage.getItem("ligatures");
+    const newLigatures = ligatures == "1" ? "0" : "1";
+
+    localStorage.setItem("ligatures", newLigatures);
+    document.documentElement.setAttribute("ligatures", newLigatures);
 }
+
+const iconLinks = [
+    {
+        title: 'GitHub',
+        icon: 'bi bi-github',
+        href: 'https://github.com/KubaZ2/NetCord'
+    },
+    {
+        title: 'Discord',
+        icon: 'bi bi-discord',
+        href: 'https://discord.gg/meaSHTGyUH'
+    },
+    {
+        title: 'Toggle ligatures',
+        icon: 'icon icon-ligatures',
+        onclick: 'window.docfx.changeLigatures()',
+    }
+];
+
+export default {
+    changeLigatures,
+    iconLinks
+};
