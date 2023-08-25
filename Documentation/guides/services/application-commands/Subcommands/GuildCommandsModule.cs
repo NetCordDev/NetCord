252cb94b0c427348a7a19d6d7b479d1ad94c0f08
KubaZ2 <jazloty@o2.pl>
2023-08-25 21:15:51 +0200

    Add subcommands docs

---
+++ Documentation/guides/services/application-commands/Subcommands/GuildCommandsModule.cs
@@ -0,0 +1,36 @@
+﻿using NetCord;
+using NetCord.Rest;
+using NetCord.Services;
+using NetCord.Services.ApplicationCommands;
+
+namespace MyBot;
+
+[SlashCommand("guild", "Guild command")]
+public class GuildCommandsModule : ApplicationCommandModule<SlashCommandContext>
+{
+    [SubSlashCommand("channels", "Get guild channel count")]
+    public Task ChannelsAsync()
+    {
+        return RespondAsync(InteractionCallback.ChannelMessageWithSource($"Channels: {Context.Guild!.Channels.Count}"));
+    }
+
+    [SubSlashCommand("name", "Guild name")]
+    public class GuildNameModule : ApplicationCommandModule<SlashCommandContext>
+    {
+        [SubSlashCommand("get", "Get guild name")]
+        public Task GetNameAsync()
+        {
+            return RespondAsync(InteractionCallback.ChannelMessageWithSource($"Name: {Context.Guild!.Name}"));
+        }
+
+        [RequireUserPermissions<SlashCommandContext>(Permissions.ManageGuild)]
+        [RequireBotPermissions<SlashCommandContext>(Permissions.ManageGuild)]
+        [SubSlashCommand("set", "Set guild name")]
+        public async Task SetNameAsync(string name)
+        {
+            var guild = Context.Guild!;
+            await guild.ModifyAsync(g => g.Name = name);
+            await RespondAsync(InteractionCallback.ChannelMessageWithSource($"Name: {guild.Name} -> {name}"));
+        }
+    }
+}
---
+++ Documentation/guides/services/application-commands/subcommands.md
@@ -0,0 +1,4 @@
+# Subcommands
+
+## Example
+[!code-cs[GuildCommandsModule.cs](Subcommands/GuildCommandsModule.cs)]
