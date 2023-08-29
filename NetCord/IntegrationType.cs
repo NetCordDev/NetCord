2347cee4210d6d650eba2a3020d39d8a917d6227
KubaZ2 <jazloty@o2.pl>
2023-08-29 15:04:03 +0200

    Change `IntegrationType` converter

--- NetCord/IntegrationType.cs
+++ NetCord/IntegrationType.cs
@@ -2,11 +2,18 @@
 
 namespace NetCord;
 
-[JsonConverter(typeof(JsonConverters.IntegrationTypeConverter))]
+[JsonConverter(typeof(JsonConverters.StringEnumConverterWithErrorHandling<IntegrationType>))]
 public enum IntegrationType
 {
+    [JsonPropertyName("twitch")]
     Twitch,
+
+    [JsonPropertyName("youtube")]
     YouTube,
+
+    [JsonPropertyName("discord")]
     Discord,
+
+    [JsonPropertyName("guild_subscription")]
     GuildSubscription,
 }
