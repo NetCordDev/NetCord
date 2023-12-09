d0d90a335f1a69d41b490429a6c1218dd48105fa
KubaZ2 <jazloty@o2.pl>
2023-12-09 17:48:37 +0100

    Snow ❄️

--- Documentation/templates/NetCord/public/main.js
+++ Documentation/templates/NetCord/public/main.js
@@ -5,12 +5,43 @@ if (!ligatures)
 
 document.documentElement.setAttribute("ligatures", ligatures);
 
-function changeLigatures() {
+function toggleLigatures() {
     const ligatures = localStorage.getItem("ligatures");
     const newLigatures = ligatures == "1" ? "0" : "1";
 
     localStorage.setItem("ligatures", newLigatures);
     document.documentElement.setAttribute("ligatures", newLigatures);
+}
+
+function removeSnowflakes() {
+    const snowflakeContainer = document.getElementById('snow-container');
+    snowflakeContainer.replaceChildren();
+}
+
+function generateSnowflakes() {
+    const snowflakeContainer = document.getElementById('snow-container');
+
+    for (let i = 0; i < 9; i++) {
+        const snowflake = document.createElement('div');
+        snowflake.className = 'bi bi-snow snowflake';
+        snowflakeContainer.appendChild(snowflake);
+    }
+}
+
+function toggleSnow() {
+    const snow = localStorage.getItem("snow");
+    let newSnow;
+    if (snow == "1") {
+        removeSnowflakes();
+        newSnow = "0";
+    }
+    else {
+        generateSnowflakes();
+        newSnow = "1";
+    }
+
+    localStorage.setItem("snow", newSnow);
+    document.documentElement.setAttribute("snow", newSnow);
 }
 
 const iconLinks = [
@@ -27,11 +58,32 @@ const iconLinks = [
     {
         title: 'Toggle ligatures',
         icon: 'icon icon-ligatures',
-        onclick: 'window.docfx.changeLigatures()',
+        onclick: 'window.docfx.toggleLigatures()',
     }
 ];
 
+const month = new Date().getMonth();
+if (month == 11) {
+    let snow = localStorage.getItem("snow");
+
+    if (!snow)
+        localStorage.setItem("snow", snow = "1");
+
+    document.documentElement.setAttribute("snow", snow);
+
+    if (snow == "1")
+        generateSnowflakes();
+
+    iconLinks.push(
+        {
+            title: 'Toggle snow',
+            icon: 'bi icon-snow',
+            onclick: 'window.docfx.toggleSnow()',
+        });
+}
+
 export default {
-    changeLigatures,
+    toggleLigatures,
+    toggleSnow,
     iconLinks
 };
