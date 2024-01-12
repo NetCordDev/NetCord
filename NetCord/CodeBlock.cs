0e3ea4e313667140a98d73b55f972e22ca4697f1
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-01-12 11:53:01 +0800

    Prioritize faster test

--- NetCord/CodeBlock.cs
+++ NetCord/CodeBlock.cs
@@ -21,7 +21,7 @@ public class CodeBlock
         
         codeBlock = null;
         
-        var isCodeBlock = text.StartsWith(prefixSuffix) && text.EndsWith(prefixSuffix) && text.Length >= $"{prefixSuffix}\n{prefixSuffix}".Length;
+        var isCodeBlock = text.Length >= $"{prefixSuffix}\n{prefixSuffix}".Length && text.StartsWith(prefixSuffix) && text.EndsWith(prefixSuffix);
         
         if (!isCodeBlock)
         {
