b132c2fac4a4001b4d20aafe96ed9a41aa331919
KubaZ2 <jazloty@o2.pl>
2024-02-29 14:02:52 +0100

    Strip unnecessary `.` to avoid putting 2 dots in a message of `RestException`

--- NetCord/Rest/RestException.cs
+++ NetCord/Rest/RestException.cs
@@ -4,17 +4,29 @@ namespace NetCord.Rest;
 
 public class RestException : Exception
 {
-    public RestException(HttpStatusCode statusCode, string reasonPhrase) : base($"Response status code does not indicate success: {(int)statusCode} ({reasonPhrase}).")
+    public RestException(HttpStatusCode statusCode, string reasonPhrase) : base(GetMessage(statusCode, reasonPhrase))
     {
         StatusCode = statusCode;
         ReasonPhrase = reasonPhrase;
     }
 
-    public RestException(HttpStatusCode statusCode, string reasonPhrase, RestError error) : base($"Response status code does not indicate success: {(int)statusCode} ({reasonPhrase}). {error.Message}.")
+    public RestException(HttpStatusCode statusCode, string reasonPhrase, RestError error) : base(GetMessage(statusCode, reasonPhrase, error.Message))
     {
         StatusCode = statusCode;
         ReasonPhrase = reasonPhrase!;
         Error = error;
+    }
+
+    private static string GetMessage(HttpStatusCode statusCode, string reasonPhrase)
+    {
+        return $"Response status code does not indicate success: {(int)statusCode} ({reasonPhrase}).";
+    }
+
+    private static string GetMessage(HttpStatusCode statusCode, string reasonPhrase, string errorMessage)
+    {
+        return errorMessage.EndsWith('.')
+                ? $"Response status code does not indicate success: {(int)statusCode} ({reasonPhrase}). {errorMessage}"
+                : $"Response status code does not indicate success: {(int)statusCode} ({reasonPhrase}). {errorMessage}.";
     }
 
     public HttpStatusCode StatusCode { get; }
