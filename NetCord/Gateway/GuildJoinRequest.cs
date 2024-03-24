5023062da9436718bc4466d0ed0daab4fc8bc1b4
KubaZ2 <jazloty@o2.pl>
2024-03-24 18:53:54 +0100

    Use a primary constructor

--- NetCord/Gateway/GuildJoinRequest.cs
+++ NetCord/Gateway/GuildJoinRequest.cs
@@ -2,27 +2,18 @@
 
 namespace NetCord.Gateway;
 
-public class GuildJoinRequest : IJsonModel<JsonModels.JsonGuildJoinRequest>
+public class GuildJoinRequest(JsonModels.JsonGuildJoinRequest jsonModel, RestClient client) : IJsonModel<JsonModels.JsonGuildJoinRequest>
 {
-    private readonly JsonModels.JsonGuildJoinRequest _jsonModel;
-    JsonModels.JsonGuildJoinRequest IJsonModel<JsonModels.JsonGuildJoinRequest>.JsonModel => _jsonModel;
+    JsonModels.JsonGuildJoinRequest IJsonModel<JsonModels.JsonGuildJoinRequest>.JsonModel => jsonModel;
     
-    public GuildJoinRequestStatus ApplicationStatus => _jsonModel.ApplicationStatus;
-    public DateTimeOffset? CreatedAt => _jsonModel.CreatedAt;
-    public ulong GuildId => _jsonModel.GuildId;
-    public DateTimeOffset LastSeenAt => _jsonModel.LastSeenAt;
-    public string? RejectionReason => _jsonModel.RejectionReason;
-    public ulong UserId => _jsonModel.UserId;
-    public User User { get; }
-    public IReadOnlyList<VerificationField> FormResponses { get; }
-    public User ActionedByUser { get; }
-    public ulong ActionedAt => _jsonModel.ActionedAt;
-    
-    public GuildJoinRequest(JsonModels.JsonGuildJoinRequest jsonModel, RestClient client)
-    {
-        _jsonModel = jsonModel;
-        User = new User(jsonModel.User, client);
-        ActionedByUser = new User(jsonModel.ActionedByUser, client);
-        FormResponses = _jsonModel.FormResponses.Select(e => new VerificationField(e)).ToArray();
-    }
+    public GuildJoinRequestStatus ApplicationStatus => jsonModel.ApplicationStatus;
+    public DateTimeOffset CreatedAt => jsonModel.CreatedAt;
+    public ulong GuildId => jsonModel.GuildId;
+    public DateTimeOffset LastSeenAt => jsonModel.LastSeenAt;
+    public string? RejectionReason => jsonModel.RejectionReason;
+    public ulong UserId => jsonModel.UserId;
+    public User User { get; } = new(jsonModel.User, client);
+    public IReadOnlyList<VerificationField> FormResponses { get; } = jsonModel.FormResponses.Select(e => new VerificationField(e)).ToArray();
+    public User ActionedByUser { get; } = new(jsonModel.ActionedByUser, client);
+    public ulong ActionedAt => jsonModel.ActionedAt;
 }
