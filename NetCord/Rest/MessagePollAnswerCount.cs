73c758aac366c87e4250628634f7d218b412ccf6
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-04-17 18:57:32 +0800

    Added MessagePollAnswerCount.cs and MessagePollResults.cs

---
+++ NetCord/Rest/MessagePollAnswerCount.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+using NetCord.JsonModels;
+
+namespace NetCord.Rest;
+
+public partial class MessagePollAnswerCount : JsonEntity
+{
+    [JsonPropertyName("id")]
+    public int AnswerId { get; set; }
+    
+    [JsonPropertyName("count")]
+    public int Count { get; set; }
+    
+    [JsonPropertyName("me_voted")]
+    public bool MeVoted { get; set; }
+}
---
+++ NetCord/Rest/MessagePollResults.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+using NetCord.JsonModels;
+
+namespace NetCord.Rest;
+
+public partial class MessagePollResults : JsonEntity
+{
+    [JsonPropertyName("is_finalized")]
+    public bool IsFinalized { get; set; }
+    
+    [JsonPropertyName("answer_counts")]
+    public MessagePollAnswerCount[] AnswerCounts { get; set; }
+}
