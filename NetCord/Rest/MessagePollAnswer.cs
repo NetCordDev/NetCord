15a093cb853858672d34e1787ea44e6b910c4f5b
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-04-17 18:52:26 +0800

    Added MessagePollAnswer.Id

--- NetCord/Rest/MessagePollAnswer.cs
+++ NetCord/Rest/MessagePollAnswer.cs
@@ -6,6 +6,9 @@ namespace NetCord.Rest;
 
 public partial class MessagePollAnswer : JsonEntity
 {
+    [JsonPropertyName("answer_id")]
+    public int? Id { get; set; }
+    
     [JsonPropertyName("poll_media")]
     public MessagePollMedia PollMedia { get; set; }
 }
