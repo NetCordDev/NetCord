20db56eb615e7d1d86d33524b9b06e4454c3e359
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-04-18 16:03:02 +0800

    MessagePoll.cs: - Fixed inproper initialization. - Use IReadOnlyList over array

--- NetCord/MessagePoll.cs
+++ NetCord/MessagePoll.cs
@@ -1,29 +1,18 @@
-﻿using NetCord.JsonModels;
+﻿using System.Collections.Immutable;
+
+using NetCord.JsonModels;
 using NetCord.Rest;
 
 namespace NetCord;
 
-public partial class MessagePoll : IJsonModel<JsonMessagePoll>
+public partial class MessagePoll(JsonMessagePoll jsonModel, ulong guildId, RestClient client) : IJsonModel<JsonMessagePoll>
 {
-    JsonMessagePoll IJsonModel<JsonMessagePoll>.JsonModel => _jsonModel;
-    private protected readonly JsonMessagePoll _jsonModel;
-    public JsonMessagePoll JsonModel { get; }
-    public MessagePollMedia Question { get; }
-    public MessagePollAnswer[] Answers { get; }
-    public bool AllowMultiselect { get; }
-    public MessagePollLayoutType LayoutType { get; }
+    JsonMessagePoll IJsonModel<JsonMessagePoll>.JsonModel => jsonModel;
+    public MessagePollMedia Question = new(jsonModel.Question, guildId, client);
+    public IReadOnlyList<MessagePollAnswer> Answers = jsonModel.Answers.Select(x => new MessagePollAnswer(x, guildId, client)).ToImmutableList();
+    public bool AllowMultiselect => jsonModel.AllowMultiselect;
+    public MessagePollLayoutType LayoutType => jsonModel.LayoutType;
     // Non-expiring posts are possible in the future, see: https://github.com/discord/discord-api-docs/blob/e4bdf50f11f9ca61ace2636285e029a2b3dfd0ec/docs/resources/Poll.md#poll-object
-    public DateTimeOffset? ExpireAt { get; }
-    public MessagePollResults Results { get; }
-    
-    public MessagePoll(JsonMessagePoll jsonModel, ulong guildId, RestClient client)
-    {
-        _jsonModel = jsonModel;
-        Question = new(jsonModel.Question, guildId, client);
-        Answers = jsonModel.Answers.Select(x => new MessagePollAnswer(x, guildId, client)).ToArray();
-        AllowMultiselect = jsonModel.AllowMultiselect;
-        LayoutType = jsonModel.LayoutType;
-        ExpireAt = jsonModel.ExpireAt;
-        Results = new(jsonModel.Results);
-    }
+    public DateTimeOffset? ExpireAt => jsonModel.ExpireAt;
+    public MessagePollResults Results = new(jsonModel.Results);
 }
