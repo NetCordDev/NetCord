a070dae7b550dd246bce5d734dcb1485936166d3
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-04-18 16:03:13 +0800

    Prefer IEnumerable over array.

--- NetCord/MessagePollProperties.cs
+++ NetCord/MessagePollProperties.cs
@@ -2,13 +2,13 @@
 
 namespace NetCord;
 
-public partial class MessagePollProperties(MessagePollMediaProperties question, MessagePollAnswerProperties[] answers)
+public partial class MessagePollProperties(MessagePollMediaProperties question, IEnumerable<MessagePollAnswerProperties> answers)
 {
     [JsonPropertyName("question")]
     public MessagePollMediaProperties Question { get; set; } = question;
 
     [JsonPropertyName("answers")]
-    public MessagePollAnswerProperties[] Answers { get; set; } = answers;
+    public IEnumerable<MessagePollAnswerProperties> Answers { get; set; } = answers;
 
     [JsonPropertyName("allow_multiselect")]
     public bool AllowMultiselect { get; set; }
