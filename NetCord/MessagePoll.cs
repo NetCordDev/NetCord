e98a32ffb816ba99db68826c9f57e264255a2d7e
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-04-18 09:03:08 +0800

    Removed MessagePoll's setters

--- NetCord/MessagePoll.cs
+++ NetCord/MessagePoll.cs
@@ -6,10 +6,10 @@ namespace NetCord;
 public partial class MessagePoll : IJsonModel<JsonMessagePoll>
 {
     public JsonMessagePoll JsonModel { get; }
-    public MessagePollMedia Question { get; set; }
+    public MessagePollMedia Question { get; }
     public MessagePollAnswer[] Answers { get; }
-    public bool AllowMultiselect { get; set; }
-    public MessagePollLayoutType LayoutType { get; set; }
+    public bool AllowMultiselect { get; }
+    public MessagePollLayoutType LayoutType { get; }
     // Non-expiring posts are possible in the future, see: https://github.com/discord/discord-api-docs/blob/e4bdf50f11f9ca61ace2636285e029a2b3dfd0ec/docs/resources/Poll.md#poll-object
     public DateTimeOffset? ExpireAt { get; }
     public MessagePollResults Results { get; }
