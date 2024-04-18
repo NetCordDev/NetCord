8ee04f0a09c7b702dba4ecd39ca3ccc8f1a11708
TrumpMcDonaldz <74057874+budgetdevv@users.noreply.github.com>
2024-04-18 17:27:38 +0800

    Use IReadOnlyList
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>

--- NetCord/MessagePollResults.cs
+++ NetCord/MessagePollResults.cs
@@ -6,6 +6,6 @@ public class MessagePollResults(JsonMessagePollResults jsonModel) : IJsonModel<J
 {
     JsonMessagePollResults IJsonModel<JsonMessagePollResults>.JsonModel => jsonModel;
     public bool IsFinalized => jsonModel.IsFinalized;
-    public MessagePollAnswerCount[]? Answers => jsonModel.Answers;
+    public IReadOnlyList<MessagePollAnswerCount>? Answers => jsonModel.Answers;
     public bool ContainsAnswers => Answers != null;
 }
