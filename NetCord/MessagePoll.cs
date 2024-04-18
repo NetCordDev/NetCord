ab2643e4130651239372d6c6300c2ee4b2fad1b9
TrumpMcDonaldz <74057874+budgetdevv@users.noreply.github.com>
2024-04-18 17:26:39 +0800

    Revert change to IReadOnlyList
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>

--- NetCord/MessagePoll.cs
+++ NetCord/MessagePoll.cs
@@ -9,7 +9,7 @@ public class MessagePoll(JsonMessagePoll jsonModel, ulong guildId, RestClient cl
 {
     JsonMessagePoll IJsonModel<JsonMessagePoll>.JsonModel => jsonModel;
     public MessagePollMedia Question = new(jsonModel.Question, guildId, client);
-    public IReadOnlyList<MessagePollAnswer> Answers = jsonModel.Answers.Select(x => new MessagePollAnswer(x, guildId, client)).ToImmutableList();
+    public IReadOnlyList<MessagePollAnswer> Answers = jsonModel.Answers.Select(x => new MessagePollAnswer(x, guildId, client)).ToArray();
     public bool AllowMultiselect => jsonModel.AllowMultiselect;
     public MessagePollLayoutType LayoutType => jsonModel.LayoutType;
     // Non-expiring posts are possible in the future, see: https://github.com/discord/discord-api-docs/blob/e4bdf50f11f9ca61ace2636285e029a2b3dfd0ec/docs/resources/Poll.md#poll-object
