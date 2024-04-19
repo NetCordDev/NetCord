4161b6e7d9ad174cef2217f3a723690aca1d7ba2
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-04-19 19:00:35 +0800

    It seems like this is only sent via gateway, commenting out for now...

--- NetCord/MessagePollAnswerProperties.cs
+++ NetCord/MessagePollAnswerProperties.cs
@@ -4,8 +4,8 @@ namespace NetCord;
 
 public partial class MessagePollAnswerProperties(MessagePollMediaProperties pollMedia)
 {
-    [JsonPropertyName("answer_id")]
-    public uint AnswerId { get; set; }
+    // [JsonPropertyName("answer_id")]
+    // public uint AnswerId { get; set; }
     
     [JsonPropertyName("poll_media")]
     public MessagePollMediaProperties PollMedia { get; set; } = pollMedia;
