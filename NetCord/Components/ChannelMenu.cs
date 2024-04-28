9d5057293820e2a833de5845c9e0bde9d5b795a9
KubaZ2 <jazloty@o2.pl>
2024-04-28 21:31:10 +0200

    Fix entity menus without default values

--- NetCord/Components/ChannelMenu.cs
+++ NetCord/Components/ChannelMenu.cs
@@ -4,7 +4,7 @@ namespace NetCord;
 
 public class ChannelMenu(JsonComponent jsonModel) : EntityMenu(jsonModel)
 {
-    public IReadOnlyList<ulong> DefaultValues { get; } = jsonModel.DefaultValues!.Select(d => d.Id).ToArray();
+    public IReadOnlyList<ulong> DefaultValues { get; } = jsonModel.DefaultValues.SelectOrEmpty(d => d.Id).ToArray();
 
     public IReadOnlyList<ChannelType> ChannelTypes => _jsonModel.ChannelTypes!;
 }
--- NetCord/Components/UserMenu.cs
+++ NetCord/Components/UserMenu.cs
@@ -4,5 +4,5 @@ namespace NetCord;
 
 public class UserMenu(JsonComponent jsonModel) : EntityMenu(jsonModel)
 {
-    public IReadOnlyList<ulong> DefaultValues { get; } = jsonModel.DefaultValues!.Select(d => d.Id).ToArray();
+    public IReadOnlyList<ulong> DefaultValues { get; } = jsonModel.DefaultValues.SelectOrEmpty(d => d.Id).ToArray();
 }
