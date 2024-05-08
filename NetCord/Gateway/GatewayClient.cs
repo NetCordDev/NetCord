0da34ddb664036ead31e0afcb5e5b5473e54e1fe
Red-K0 <114494949+Red-K0@users.noreply.github.com>
2024-05-09 02:15:41 +0300

    Merge conflict fix, updated Ready event

--- NetCord/Gateway/GatewayClient.cs
+++ NetCord/Gateway/GatewayClient.cs
@@ -24,7 +24,7 @@ public partial class GatewayClient : WebSocketClient, IEntity
 
     /// <summary>
     /// The ready event is dispatched when a client has completed the initial handshake with the gateway (for new sessions).
-    /// The ready event can be the largest and most complex event the gateway will send, as it contains all the state required for a client to begin interacting with the rest of the platform.<br/>
+    /// The ready event, while generally small in practice, can be the largest and most complex event the gateway will send, as it contains all the state required for a client to begin interacting with the rest of the platform.<br/>
     /// <br/> Required Intents: None
     /// <br/> Optional Intents: None
     /// </summary>
@@ -326,14 +326,14 @@ public partial class GatewayClient : WebSocketClient, IEntity
     /// <br/> Required Intents: <see cref="GatewayIntents.Guilds"/>
     /// <br/> Optional Intents: None
     /// </summary>
-    public event Func<RoleEventArgs, ValueTask>? RoleCreate;
+    public event Func<Role, ValueTask>? RoleCreate;
 
     /// <summary>
     /// Sent when a guild role is updated.<br/>
     /// <br/> Required Intents: <see cref="GatewayIntents.Guilds"/>
     /// <br/> Optional Intents: None
     /// </summary>
-    public event Func<RoleEventArgs, ValueTask>? RoleUpdate;
+    public event Func<Role, ValueTask>? RoleUpdate;
 
     /// <summary>
     /// Sent when a guild role is deleted.<br/>
