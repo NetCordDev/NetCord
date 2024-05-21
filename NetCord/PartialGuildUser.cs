215c48f70d87b01825e37fdac1bdcca7dba6e123
Red-K0 <114494949+Red-K0@users.noreply.github.com>
2024-05-21 23:20:26 +0300

    User.cs and PartialGuildUser.cs fully documented.

--- NetCord/PartialGuildUser.cs
+++ NetCord/PartialGuildUser.cs
@@ -3,22 +3,71 @@ using NetCord.Rest;
 
 namespace NetCord;
 
+/// <summary>
+/// Represents a <see cref="GuildUser"/> object that lacks a <see cref="GuildUser.GuildId"/> field, as well as methods relying on it.
+/// </summary>
 public class PartialGuildUser(JsonGuildUser jsonModel, RestClient client) : User(jsonModel.User, client), IJsonModel<JsonGuildUser>
 {
     JsonGuildUser IJsonModel<JsonGuildUser>.JsonModel => _jsonModel;
     private protected new readonly JsonGuildUser _jsonModel = jsonModel;
 
+    /// <summary>
+    /// This user's guild nickname.
+    /// </summary>
     public string? Nickname => _jsonModel.Nickname;
+
+    /// <summary>
+    /// The user's guild avatar hash.
+    /// </summary>
     public string? GuildAvatarHash => _jsonModel.GuildAvatarHash;
+
+    /// <summary>
+    /// Array of role object ids.
+    /// </summary>
     public IReadOnlyList<ulong> RoleIds => _jsonModel.RoleIds;
+
+    /// <summary>
+    /// The ID of the user's hoisted role, used to categorize the user in the member list.
+    /// </summary>
     public ulong? HoistedRoleId => _jsonModel.HoistedRoleId;
+
+    /// <summary>
+    /// When the user joined the guild.
+    /// </summary>
     public DateTimeOffset JoinedAt => _jsonModel.JoinedAt;
+
+    /// <summary>
+    /// When the user started boosting the guild.
+    /// </summary>
     public DateTimeOffset? GuildBoostStart => _jsonModel.GuildBoostStart;
+
+    /// <summary>
+    /// Whether the user is deafened in voice channels.
+    /// </summary>
     public bool Deafened => _jsonModel.Deafened;
+
+    /// <summary>
+    /// Whether the user is muted in voice channels.
+    /// </summary>
     public bool Muted => _jsonModel.Muted;
+
+    /// <summary>
+    /// Guild member flags represented as a bit set, defaults to 0.
+    /// </summary>
     public GuildUserFlags GuildFlags => _jsonModel.GuildFlags;
+
+    /// <summary>
+    /// Whether the user has not yet passed the guild's Membership Screening requirements.
+    /// </summary>
     public bool? IsPending => _jsonModel.IsPending;
+
+    /// <summary>
+    /// When the user's timeout will expire and the user will be able to communicate in the guild again, null or a time in the past if the user is not timed out.
+    /// </summary>
     public DateTimeOffset? TimeOutUntil => _jsonModel.TimeOutUntil;
 
+    /// <summary>
+    /// Whether the user has a set guild avatar.
+    /// </summary>
     public bool HasGuildAvatar => GuildAvatarHash is not null;
 }
--- NetCord/User.cs
+++ NetCord/User.cs
@@ -3,140 +3,191 @@
 namespace NetCord;
 
 /// <summary>
-/// 
+/// Represents a user of any interactable resource on Discord.
 /// </summary>
+/// <remarks>
+/// Users in Discord are generally considered the base entity and can be members of guilds, participate in text and voice chat, and much more. Users are separated by a distinction of 'bot' vs 'normal'. Bot users are automated users that are 'owned' by another user.
+/// </remarks>
 public partial class User(JsonModels.JsonUser jsonModel, RestClient client) : ClientEntity(client), IJsonModel<JsonModels.JsonUser>
 {
     JsonModels.JsonUser IJsonModel<JsonModels.JsonUser>.JsonModel => _jsonModel;
     private protected readonly JsonModels.JsonUser _jsonModel = jsonModel;
 
     /// <summary>
-    /// The user's ID. Requires the <c>identify</c> OAuth2 scope.
+    /// The user's ID.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public override ulong Id => _jsonModel.Id;
 
     /// <summary>
-    /// The user's username, not unique across the platform.
+    /// The user's username, not unique across the platform. Restrictions:
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>
+    ///         Must be between 2 and 32 characters long.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///         Usernames cannot contain the following substrings: <c>"@"</c>, <c>"#"</c>, <c>":"</c>, <c>"```"</c>, <c>"discord"</c>.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///         Usernames cannot be: <c>"everyone"</c> or <c>"here"</c>.
+    ///         </description>
+    ///     </item>
+    /// </list>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public string Username => _jsonModel.Username;
 
     /// <summary>
-    /// The user's Discord-tag.
+    /// The user's Discord-tag.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public ushort Discriminator => _jsonModel.Discriminator;
 
     /// <summary>
-    /// The user's display name, if it is set. For bots, this is the application name.
+    /// The user's display name, if it is set. For bots, this is the application name. Restrictions:
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>
+    ///         Must be between 1 and 32 characters long.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///         Some zero-width and non-rendering characters are limited.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///         Names are sanitized and trimmed of leading, trailing, and excessive internal whitespace.
+    ///         </description>
+    ///     </item>
+    /// </list>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public string? GlobalName => _jsonModel.GlobalName;
 
     /// <summary>
-    /// The user's avatar hash.
+    /// The user's avatar hash.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public string? AvatarHash => _jsonModel.AvatarHash;
 
     /// <summary>
-    /// Whether the user belongs to an OAuth2 application.
+    /// Whether the user belongs to an OAuth2 application.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public bool IsBot => _jsonModel.IsBot;
 
     /// <summary>
-    /// Whether the user is an Official Discord System user (part of the urgent message system).
+    /// Whether the user is an Official Discord System user (part of the urgent message system).<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public bool? IsSystemUser => _jsonModel.IsSystemUser;
 
     /// <summary>
-    /// Whether the user has two factor enabled on their account.
+    /// Whether the user has two factor enabled on their account.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public bool? MfaEnabled => _jsonModel.MfaEnabled;
 
     /// <summary>
-    /// The user's banner hash.
+    /// The user's banner hash.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public string? BannerHash => _jsonModel.BannerHash;
 
     /// <summary>
-    /// The user's banner color encoded as an integer representation of hexadecimal color code.
+    /// The user's banner color encoded as an integer representation of hexadecimal color code.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public Color? AccentColor => _jsonModel.AccentColor;
 
     /// <summary>
-    /// The user's chosen language option.
+    /// The user's chosen language option.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public string? Locale => _jsonModel.Locale;
 
     /// <summary>
-    /// Whether the email on this account has been verified.
+    /// Whether the email on this account has been verified.<br/>
+    /// Requires the <c>email</c> OAuth2 scope.
     /// </summary>
     public bool? Verified => _jsonModel.Verified;
 
     /// <summary>
-    /// The user's email.
+    /// The user's email.<br/>
+    /// Requires the <c>email</c> OAuth2 scope.
     /// </summary>
     public string? Email => _jsonModel.Email;
 
     /// <summary>
-    /// The flags on a user's account.
+    /// The flags on a user's account.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public UserFlags? Flags => _jsonModel.Flags;
 
     /// <summary>
-    /// The type of Nitro subscription on a user's account.
+    /// The type of Nitro subscription on a user's account.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public PremiumType? PremiumType => _jsonModel.PremiumType;
 
     /// <summary>
-    /// The public flags on a user's account.
+    /// The public flags on a user's account.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public UserFlags? PublicFlags => _jsonModel.PublicFlags;
 
     /// <summary>
-    /// The user's avatar decoration hash.
+    /// The user's avatar decoration hash.<br/>
+    /// Requires the <c>identify</c> OAuth2 scope.
     /// </summary>
     public string? AvatarDecorationHash => _jsonModel.AvatarDecorationHash;
 
     /// <summary>
-    /// Whether the user has set a custom avatar.
+    /// Whether the user has a set custom avatar.
     /// </summary>
     public bool HasAvatar => AvatarHash is not null;
 
     /// <summary>
-    /// 
+    /// Returns an <see cref="ImageUrl"/> object representing the user's avatar.
     /// </summary>
     public ImageUrl GetAvatarUrl(ImageFormat? format = null) => ImageUrl.UserAvatar(Id, AvatarHash!, format);
 
     /// <summary>
-    /// 
+    /// Whether the user has a set custom banner image.
     /// </summary>
     public bool HasBanner => BannerHash is not null;
 
     /// <summary>
-    /// 
+    /// Returns an <see cref="ImageUrl"/> object representing the user's banner.
     /// </summary>
     public ImageUrl GetBannerUrl(ImageFormat? format = null) => ImageUrl.UserBanner(Id, BannerHash!, format);
 
     /// <summary>
-    /// 
+    /// Whether the user has a set avatar decoration.
     /// </summary>
     public bool HasAvatarDecoration => AvatarDecorationHash is not null;
 
     /// <summary>
-    /// 
+    /// Returns an <see cref="ImageUrl"/> object representing the user's avatar decoration.
     /// </summary>
     public ImageUrl GetAvatarDecorationUrl() => ImageUrl.UserAvatarDecoration(Id, AvatarDecorationHash!);
 
     /// <summary>
-    /// 
+    /// Returns an <see cref="ImageUrl"/> object representing the user's default avatar.
     /// </summary>
     public ImageUrl DefaultAvatarUrl => Discriminator is 0 ? ImageUrl.DefaultUserAvatar(Id) : ImageUrl.DefaultUserAvatar(Discriminator);
 
     /// <summary>
-    /// 
+    /// Converts the ID of this user into its string representation, using Discord's mention syntax.
     /// </summary>
     public override string ToString() => $"<@{Id}>";
 
-    /// <summary>
-    /// 
-    /// </summary>
     public override bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format = default, IFormatProvider? provider = null) => Mention.TryFormatUser(destination, out charsWritten, Id);
 }
