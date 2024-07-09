3008a9b8e2317e13ab95809b0804634f93a78bf0
KubaZ2 <jazloty@o2.pl>
2024-07-09 10:21:55 +0200

    Add missing `GenerateAliasAttribute`s

--- NetCord/Rest/RestClient.Poll.cs
+++ NetCord/Rest/RestClient.Poll.cs
@@ -4,6 +4,7 @@ namespace NetCord.Rest;
 
 public partial class RestClient
 {
+    [GenerateAlias([typeof(TextChannel)], nameof(TextChannel.Id))]
     [GenerateAlias([typeof(RestMessage), typeof(IPartialMessage)], nameof(RestMessage.ChannelId), nameof(RestMessage.Id), TypeNameOverride = nameof(Message))]
     public IAsyncEnumerable<User> GetMessagePollAnswerVotersAsync(ulong channelId, ulong messageId, int answerId, PaginationProperties<ulong>? paginationProperties = null, RestRequestProperties? properties = null)
     {
@@ -21,6 +22,7 @@ public partial class RestClient
             properties);
     }
 
+    [GenerateAlias([typeof(TextChannel)], nameof(TextChannel.Id))]
     [GenerateAlias([typeof(RestMessage), typeof(IPartialMessage)], nameof(RestMessage.ChannelId), nameof(RestMessage.Id), TypeNameOverride = nameof(Message))]
     public async Task<RestMessage> EndMessagePollAsync(ulong channelId, ulong messageId, RestRequestProperties? properties = null)
         => new(await (await SendRequestAsync(HttpMethod.Post, $"/channels/{channelId}/polls/{messageId}/expire", null, new(channelId), properties).ConfigureAwait(false)).ToObjectAsync(Serialization.Default.JsonMessage).ConfigureAwait(false), this);
