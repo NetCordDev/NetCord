c0584b38c51e24a9c7b9a02c5aadefd51957b820
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2024-08-06 20:10:26 +0200

    Swap TTS with components, oopsie

--- NetCord/Rest/IMessageProperties.cs
+++ NetCord/Rest/IMessageProperties.cs
@@ -2,8 +2,6 @@
 
 public interface IMessageProperties
 {
-    public bool Tts { get; set; }
-
     public string? Content { get; set; }
 
     public IEnumerable<EmbedProperties>? Embeds { get; set; }
@@ -11,6 +9,8 @@ public interface IMessageProperties
     public AllowedMentionsProperties? AllowedMentions { get; set; }
 
     public IEnumerable<AttachmentProperties>? Attachments { get; set; }
+
+    public IEnumerable<MessageComponentProperties>? Components { get; set; }
 
     public MessageFlags? Flags { get; set; }
 }
