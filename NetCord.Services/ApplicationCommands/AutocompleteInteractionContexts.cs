15e3fb368bbfaa18fc8c96b118a9a45a4b32378f
Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
2024-10-01 22:04:08 +0200

    Improve contexts (#54)
    * Fix the type for `InteractionGuildReference.Features`
    
    * Improve contexts and refactor them

--- NetCord.Services/ApplicationCommands/AutocompleteInteractionContexts.cs
+++ NetCord.Services/ApplicationCommands/AutocompleteInteractionContexts.cs
@@ -4,12 +4,17 @@ namespace NetCord.Services.ApplicationCommands;
 
 public class BaseAutocompleteInteractionContext(AutocompleteInteraction interaction) : IAutocompleteInteractionContext
 {
-    public virtual AutocompleteInteraction Interaction { get; } = interaction;
+    public AutocompleteInteraction Interaction => interaction;
 }
 
-public class AutocompleteInteractionContext(AutocompleteInteraction interaction, GatewayClient client) : BaseAutocompleteInteractionContext(interaction), IGatewayClientContext, IGuildContext, IChannelContext, IUserContext
+public class AutocompleteInteractionContext(AutocompleteInteraction interaction, GatewayClient client)
+    : BaseAutocompleteInteractionContext(interaction),
+      IGatewayClientContext,
+      IGuildContext,
+      IChannelContext,
+      IUserContext
 {
-    public GatewayClient Client { get; } = client;
+    public GatewayClient Client => client;
     public Guild? Guild => Interaction.Guild;
     public TextChannel Channel => Interaction.Channel;
     public User User => Interaction.User;
