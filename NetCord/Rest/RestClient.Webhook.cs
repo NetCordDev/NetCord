b36104ed75cbae43e7a72962b0c4d86977967308
KubaZ2 <jazloty@o2.pl>
2025-04-19 17:14:11 +0200

    Enhance IncomingWebhook and update RestClient methods
    - Added `ToClient` method to `IncomingWebhook` for converting to `WebhookClient`.
    - Updated `Token` property to be non-nullable.
    - Modified `CreateWebhookAsync` in `RestClient` to return `IncomingWebhook` instead of generic `Webhook`, directly creating it from API response.

--- NetCord/Rest/RestClient.Webhook.cs
+++ NetCord/Rest/RestClient.Webhook.cs
@@ -6,10 +6,10 @@ public partial class RestClient
 {
     [GenerateAlias([typeof(ForumGuildChannel)], nameof(ForumGuildChannel.Id))]
     [GenerateAlias([typeof(TextGuildChannel)], nameof(TextGuildChannel.Id))]
-    public async Task<Webhook> CreateWebhookAsync(ulong channelId, WebhookProperties webhookProperties, RestRequestProperties? properties = null, CancellationToken cancellationToken = default)
+    public async Task<IncomingWebhook> CreateWebhookAsync(ulong channelId, WebhookProperties webhookProperties, RestRequestProperties? properties = null, CancellationToken cancellationToken = default)
     {
         using (HttpContent content = new JsonContent<WebhookProperties>(webhookProperties, Serialization.Default.WebhookProperties))
-            return Webhook.CreateFromJson(await (await SendRequestAsync(HttpMethod.Post, content, $"/channels/{channelId}/webhooks", null, new(channelId), properties, cancellationToken: cancellationToken).ConfigureAwait(false)).ToObjectAsync(Serialization.Default.JsonWebhook).ConfigureAwait(false), this);
+            return new(await (await SendRequestAsync(HttpMethod.Post, content, $"/channels/{channelId}/webhooks", null, new(channelId), properties, cancellationToken: cancellationToken).ConfigureAwait(false)).ToObjectAsync(Serialization.Default.JsonWebhook).ConfigureAwait(false), this);
     }
 
     [GenerateAlias([typeof(ForumGuildChannel)], nameof(ForumGuildChannel.Id))]
