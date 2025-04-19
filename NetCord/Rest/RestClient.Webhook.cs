c7150ef833af0e661a137a0909e8613218f7f9ee
KubaZ2 <jazloty@o2.pl>
2025-04-19 16:27:03 +0200

    Refactor channel classes and enhance webhook functionality
    Updated `StageGuildChannel` and `VoiceGuildChannel` to inherit from `TextGuildChannel`, removing several properties to streamline the class hierarchy. Added `GetChannelWebhooksAsync` method in `RestClient.Webhook` for improved webhook management. Revised webhook creation logic in the `Webhook` class for better clarity and consistency.

--- NetCord/Rest/RestClient.Webhook.cs
+++ NetCord/Rest/RestClient.Webhook.cs
@@ -4,6 +4,7 @@ namespace NetCord.Rest;
 
 public partial class RestClient
 {
+    [GenerateAlias([typeof(ForumGuildChannel)], nameof(ForumGuildChannel.Id))]
     [GenerateAlias([typeof(TextGuildChannel)], nameof(TextGuildChannel.Id))]
     public async Task<Webhook> CreateWebhookAsync(ulong channelId, WebhookProperties webhookProperties, RestRequestProperties? properties = null, CancellationToken cancellationToken = default)
     {
@@ -11,6 +12,7 @@ public partial class RestClient
             return Webhook.CreateFromJson(await (await SendRequestAsync(HttpMethod.Post, content, $"/channels/{channelId}/webhooks", null, new(channelId), properties, cancellationToken: cancellationToken).ConfigureAwait(false)).ToObjectAsync(Serialization.Default.JsonWebhook).ConfigureAwait(false), this);
     }
 
+    [GenerateAlias([typeof(ForumGuildChannel)], nameof(ForumGuildChannel.Id))]
     [GenerateAlias([typeof(TextGuildChannel)], nameof(TextGuildChannel.Id))]
     public async Task<IReadOnlyList<Webhook>> GetChannelWebhooksAsync(ulong channelId, RestRequestProperties? properties = null, CancellationToken cancellationToken = default)
         => (await (await SendRequestAsync(HttpMethod.Get, $"/channels/{channelId}/webhooks", null, new(channelId), properties, cancellationToken: cancellationToken).ConfigureAwait(false)).ToObjectAsync(Serialization.Default.JsonWebhookArray).ConfigureAwait(false)).Select(w => Webhook.CreateFromJson(w, this)).ToArray();
