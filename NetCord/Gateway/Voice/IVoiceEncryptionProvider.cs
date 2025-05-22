3462b05a40b6ebe8481023a1f439e57cbc25fe00
KubaZ2 <jazloty@o2.pl>
2025-05-22 20:08:35 +0200

    Implement voice encryption provider

---
+++ NetCord/Gateway/Voice/IVoiceEncryptionProvider.cs
@@ -0,0 +1,8 @@
+﻿using NetCord.Gateway.Voice.Encryption;
+
+namespace NetCord.Gateway.Voice;
+
+public interface IVoiceEncryptionProvider
+{
+    public IVoiceEncryption GetEncryption(IReadOnlyList<string> modes);
+}
