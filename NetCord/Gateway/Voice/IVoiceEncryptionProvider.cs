2066e0b2d6320a616b34f7305c46eb20b9d90e89
KubaZ2 <jazloty@o2.pl>
2025-05-22 20:39:28 +0200

    Add xml comments

--- NetCord/Gateway/Voice/IVoiceEncryptionProvider.cs
+++ NetCord/Gateway/Voice/IVoiceEncryptionProvider.cs
@@ -2,7 +2,15 @@
 
 namespace NetCord.Gateway.Voice;
 
+/// <summary>
+/// Voice encryption provider.
+/// </summary>
 public interface IVoiceEncryptionProvider
 {
+    /// <summary>
+    /// Selects and returns an appropriate voice encryption implementation based on the provided encryption modes.
+    /// </summary>
+    /// <param name="modes">A list of encryption modes supported by the Discord backend.</param>
+    /// <returns>An implementation of <see cref="IVoiceEncryption"/> that matches one of the specified modes.</returns>
     public IVoiceEncryption GetEncryption(IReadOnlyList<string> modes);
 }
