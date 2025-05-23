111978a382589f9eed68c5f7b1da33b051bce60a
Red-K0 <114494949+Red-K0@users.noreply.github.com>
2025-05-23 23:29:52 +0300

    Full documentation for ApplicationCommand*.cs files (#94)
    * Full documentation for ApplicationCommand*.cs files
    
    * fix newline
    
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
    
    * Changed documentation for ApplicationCommandInteractionData.cs
    
    * Added note for autocomplete interactions
    
    * Update NetCord/ApplicationCommandInteractionDataOption.cs
    
    ---------
    
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>

--- NetCord/ApplicationCommandInteraction.cs
+++ NetCord/ApplicationCommandInteraction.cs
@@ -3,11 +3,17 @@ using NetCord.Rest;
 
 namespace NetCord;
 
+/// <summary>
+/// Acts as a base class for application commands, such as slash commands and message commands.
+/// </summary>
 public abstract class ApplicationCommandInteraction : Interaction
 {
     private protected ApplicationCommandInteraction(JsonModels.JsonInteraction jsonModel, Guild? guild, Func<IInteraction, InteractionCallback, RestRequestProperties?, CancellationToken, Task> sendResponseAsync, RestClient client) : base(jsonModel, guild, sendResponseAsync, client)
     {
     }
 
+    /// <summary>
+    /// Holds the containing application command's data.
+    /// </summary>
     public abstract override ApplicationCommandInteractionData Data { get; }
 }
