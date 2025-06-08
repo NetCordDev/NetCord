af4fa98d54a077b2ae14aa14811d9e34688031f9
KubaZ2 <jazloty@o2.pl>
2025-06-08 23:30:03 +0200

    Add xml docs for component interactions

--- NetCord.Services/ComponentInteractions/ComponentInteractionParameterAttribute.cs
+++ NetCord.Services/ComponentInteractions/ComponentInteractionParameterAttribute.cs
@@ -2,11 +2,20 @@
 
 namespace NetCord.Services.ComponentInteractions;
 
+/// <summary>
+/// Specifies metadata for a parameter of a component interaction.
+/// </summary>
 [AttributeUsage(AttributeTargets.Parameter)]
 public class ComponentInteractionParameterAttribute : Attribute
 {
+    /// <summary>
+    /// Name of the parameter.
+    /// </summary>
     public string? Name { get; init; }
 
+    /// <summary>
+    /// Type reader for the parameter, used to convert the input value to the specified type.
+    /// </summary>
     [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
     public Type? TypeReaderType { get; init; }
 }
