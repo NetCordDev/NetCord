c92ae86a160e97ed39a75f857026d5d87ac9695a
KubaZ2 <jazloty@o2.pl>
2025-06-08 23:56:42 +0200

    Remove AttributeUsage

--- NetCord.Services/ApplicationCommands/MessageCommandAttribute.cs
+++ NetCord.Services/ApplicationCommands/MessageCommandAttribute.cs
@@ -1,7 +1,6 @@
 ﻿namespace NetCord.Services.ApplicationCommands;
 
 /// <inheritdoc cref="Rest.MessageCommandProperties" />
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
 public class MessageCommandAttribute(string name) : ApplicationCommandAttribute(name)
 {
 }
--- NetCord.Services/ApplicationCommands/UserCommandAttribute.cs
+++ NetCord.Services/ApplicationCommands/UserCommandAttribute.cs
@@ -1,7 +1,6 @@
 ﻿namespace NetCord.Services.ApplicationCommands;
 
 /// <inheritdoc cref="Rest.UserCommandProperties" />
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
 public class UserCommandAttribute(string name) : ApplicationCommandAttribute(name)
 {
 }
