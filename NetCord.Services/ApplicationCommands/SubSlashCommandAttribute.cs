dd9ebbf4db3874f259adab5b502b509a54575688
KubaZ2 <jazloty@o2.pl>
2025-06-08 22:35:47 +0200

    Add xml comments for `SubSlashCommandAttribute`

--- NetCord.Services/ApplicationCommands/SubSlashCommandAttribute.cs
+++ NetCord.Services/ApplicationCommands/SubSlashCommandAttribute.cs
@@ -1,9 +1,20 @@
 ﻿namespace NetCord.Services.ApplicationCommands;
 
+/// <summary>
+/// Sub slash command allowing to create nested slash commands within a slash command.
+/// </summary>
+/// <param name="name"><inheritdoc cref="Name" path="/summary" /></param>
+/// <param name="description"><inheritdoc cref="Description" path="/summary" /></param>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
 public class SubSlashCommandAttribute(string name, string description) : Attribute
 {
+    /// <summary>
+    /// Name of the sub slash command (1-32 characters).
+    /// </summary>
     public string Name { get; } = name;
 
+    /// <summary>
+    /// Description of the sub slash command (1-100 characters).
+    /// </summary>
     public string Description { get; } = description;
 }
