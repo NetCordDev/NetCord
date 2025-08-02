a13e35531ace844821015e64f07448910e86891f
KubaZ2 <jazloty@o2.pl>
2025-08-02 16:39:14 +0200

    Add more test cases

--- Tests/ServicesTest/ApplicationCommandServiceTests.cs
+++ Tests/ServicesTest/ApplicationCommandServiceTests.cs
@@ -13,5 +13,15 @@ public class ApplicationCommandServiceTests : ServiceTests<ApplicationCommandSer
     public async ValueTask ParameterMatching()
     {
         await _tester.ExecuteAsync("test", [null, "xd"], ResultHandler.DataMatch(((string?)null, "xd")), (string? s1 = null, string? s2 = null) => Body.Data((s1, s2))).ConfigureAwait(false);
+
+        await _tester.ExecuteAsync("test", [null, "xd", null], ResultHandler.DataMatch(((string?)null, "xd", (string?)null)), (string? s1 = null, string? s2 = null, string? s3 = null) => Body.Data((s1, s2, s3))).ConfigureAwait(false);
+
+        await _tester.ExecuteAsync("test", [null, null, null, null, null, null, null, null, null], ResultHandler.DataMatch(((string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null)), (string? s1 = null, string? s2 = null, string? s3 = null, string? s4 = null, string? s5 = null, string? s6 = null, string? s7 = null, string? s8 = null, string? s9 = null) => Body.Data((s1, s2, s3, s4, s5, s6, s7, s8, s9))).ConfigureAwait(false);
+
+        await _tester.ExecuteAsync("test", [null, null, null, null, null, null, null, null, "test"], ResultHandler.DataMatch(((string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, "test")), (string? s1 = null, string? s2 = null, string? s3 = null, string? s4 = null, string? s5 = null, string? s6 = null, string? s7 = null, string? s8 = null, string? s9 = null) => Body.Data((s1, s2, s3, s4, s5, s6, s7, s8, s9))).ConfigureAwait(false);
+
+        await _tester.ExecuteAsync("test", [null, null, null, null, null, null, "test", null, null], ResultHandler.DataMatch(((string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, "test", (string?)null, (string?)null)), (string? s1 = null, string? s2 = null, string? s3 = null, string? s4 = null, string? s5 = null, string? s6 = null, string? s7 = null, string? s8 = null, string? s9 = null) => Body.Data((s1, s2, s3, s4, s5, s6, s7, s8, s9))).ConfigureAwait(false);
+
+        await _tester.ExecuteAsync("test", [null, null, null, null, null, null, "test1", "test2", "test3"], ResultHandler.DataMatch(((string?)null, (string?)null, (string?)null, (string?)null, (string?)null, (string?)null, "test1", "test2", "test3")), (string? s1 = null, string? s2 = null, string? s3 = null, string? s4 = null, string? s5 = null, string? s6 = null, string? s7 = null, string? s8 = null, string? s9 = null) => Body.Data((s1, s2, s3, s4, s5, s6, s7, s8, s9))).ConfigureAwait(false);
     }
 }
--- Tests/ServicesTest/ComponentInteractionServiceTests.cs
+++ Tests/ServicesTest/ComponentInteractionServiceTests.cs
@@ -13,5 +13,15 @@ public class ComponentInteractionServiceTests : ServiceTests<ComponentInteractio
     public async ValueTask ParameterMatching()
     {
         await _tester.ExecuteRawAsync("test", "test::xd", ResultHandler.DataMatch(((string?)null, "xd")), (string? s1 = null, string? s2 = null) => Body.Data((s1, s2))).ConfigureAwait(false);
+
+        await _tester.ExecuteRawAsync("test", "test::xd::", ResultHandler.DataMatch(((string?)null, "xd::")), (string? s1 = null, string? s2 = null) => Body.Data((s1, s2))).ConfigureAwait(false);
+
+        await _tester.ExecuteRawAsync("test", "test::::::::", ResultHandler.DataMatch(":::::::"), (string s) => Body.Data(s)).ConfigureAwait(false);
+
+        // Even though the last parameter is remainder, when it's empty and the parameter is optional, it will be set to the default parameter value
+        await _tester.ExecuteRawAsync("test", "test:", ResultHandler.DataMatch((string?)null), (string? s = null) => Body.Data(s)).ConfigureAwait(false);
+
+        // https://github.com/dotnet/roslyn/issues/79752
+        //await _tester.ExecuteRawAsync("test", "test:", ResultHandler.DataMatch(Array.Empty<string>()), (params string[] s) => Body.Data(s)).ConfigureAwait(false);
     }
 }
