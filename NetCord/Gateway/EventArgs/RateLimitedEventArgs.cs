c309b083ae448e90ff94717fe9f9edd1f456dd1c
KubaZ2 <jazloty@o2.pl>
2025-08-17 22:05:17 +0200

    Add xml docs

--- NetCord/Gateway/EventArgs/RateLimitedEventArgs.cs
+++ NetCord/Gateway/EventArgs/RateLimitedEventArgs.cs
@@ -8,10 +8,19 @@ public class RateLimitedEventArgs(JsonRateLimitedEventArgs jsonModel) : IJsonMod
 {
     JsonRateLimitedEventArgs IJsonModel<JsonRateLimitedEventArgs>.JsonModel => jsonModel;
 
+    /// <summary>
+    /// Gateway opcode of the event that was rate limited.
+    /// </summary>
     public GatewayOpcode Opcode => jsonModel.Opcode;
 
+    /// <summary>
+    /// The number of seconds to wait before submitting another request.
+    /// </summary>
     public double RetryAfter => jsonModel.RetryAfter;
 
+    /// <summary>
+    /// Metadata for the event that was rate limited.
+    /// </summary>
     public RateLimitMetadata Metadata { get; } = jsonModel.Opcode switch
     {
         GatewayOpcode.RequestGuildUsers => new RequestGuildUsersRateLimitMetadata(jsonModel.Metadata.ToObject(Serialization.Default.JsonRequestGuildUsersRateLimitMetadata)),
@@ -23,13 +32,22 @@ public abstract class RateLimitMetadata;
 
 public class RequestGuildUsersRateLimitMetadata(JsonRequestGuildUsersRateLimitMetadata jsonModel) : RateLimitMetadata
 {
+    /// <summary>
+    /// The ID of the guild to get users for.
+    /// </summary>
     public ulong GuildId => jsonModel.GuildId;
 
+    /// <summary>
+    /// The nonce to identify the response.
+    /// </summary>
     public string? Nonce => jsonModel.Nonce;
 }
 
 public interface IUnknownRateLimitMetadata
 {
+    /// <summary>
+    /// The raw data of the rate limit metadata.
+    /// </summary>
     public JsonElement Data { get; }
 }
 
