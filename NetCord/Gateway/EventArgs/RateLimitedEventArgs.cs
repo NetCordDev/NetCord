418afd85edc03df1365e0cd9bef3681d5eae5332
Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
2025-08-17 23:08:39 +0300

    Add a RateLimited Gateway event (#155)
    * Add a RateLimited Gateway event
    
    * Implement IUnknownRateLimitMetadata in UnknownRateLimitMetadata
    
    Updated the `UnknownRateLimitMetadata` class to implement the `IUnknownRateLimitMetadata` interface. Added a new property, `Data`, that returns the `data` field of type `JsonElement`, enhancing the class's functionality.
    
    * Add xml docs

---
+++ NetCord/Gateway/EventArgs/RateLimitedEventArgs.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+
+using NetCord.Gateway.JsonModels.EventArgs;
+
+namespace NetCord.Gateway;
+
+public class RateLimitedEventArgs(JsonRateLimitedEventArgs jsonModel) : IJsonModel<JsonRateLimitedEventArgs>
+{
+    JsonRateLimitedEventArgs IJsonModel<JsonRateLimitedEventArgs>.JsonModel => jsonModel;
+
+    /// <summary>
+    /// Gateway opcode of the event that was rate limited.
+    /// </summary>
+    public GatewayOpcode Opcode => jsonModel.Opcode;
+
+    /// <summary>
+    /// The number of seconds to wait before submitting another request.
+    /// </summary>
+    public double RetryAfter => jsonModel.RetryAfter;
+
+    /// <summary>
+    /// Metadata for the event that was rate limited.
+    /// </summary>
+    public RateLimitMetadata Metadata { get; } = jsonModel.Opcode switch
+    {
+        GatewayOpcode.RequestGuildUsers => new RequestGuildUsersRateLimitMetadata(jsonModel.Metadata.ToObject(Serialization.Default.JsonRequestGuildUsersRateLimitMetadata)),
+        _ => new UnknownRateLimitMetadata(jsonModel.Metadata),
+    };
+}
+
+public abstract class RateLimitMetadata;
+
+public class RequestGuildUsersRateLimitMetadata(JsonRequestGuildUsersRateLimitMetadata jsonModel) : RateLimitMetadata
+{
+    /// <summary>
+    /// The ID of the guild to get users for.
+    /// </summary>
+    public ulong GuildId => jsonModel.GuildId;
+
+    /// <summary>
+    /// The nonce to identify the response.
+    /// </summary>
+    public string? Nonce => jsonModel.Nonce;
+}
+
+public interface IUnknownRateLimitMetadata
+{
+    /// <summary>
+    /// The raw data of the rate limit metadata.
+    /// </summary>
+    public JsonElement Data { get; }
+}
+
+internal class UnknownRateLimitMetadata(JsonElement data) : RateLimitMetadata, IUnknownRateLimitMetadata
+{
+    public JsonElement Data => data;
+}
