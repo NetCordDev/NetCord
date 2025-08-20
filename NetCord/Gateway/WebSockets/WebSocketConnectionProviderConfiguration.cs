7332f29ff5df86e55f08e08fadb26b1e4a3a39e3
KubaZ2 <jazloty@o2.pl>
2025-08-20 10:45:16 +0200

    Add a xml doc for `WebSocketConnectionProviderConfiguration.ConfigureConnectionOptions`

--- NetCord/Gateway/WebSockets/WebSocketConnectionProviderConfiguration.cs
+++ NetCord/Gateway/WebSockets/WebSocketConnectionProviderConfiguration.cs
@@ -4,5 +4,8 @@ namespace NetCord.Gateway.WebSockets;
 
 public class WebSocketConnectionProviderConfiguration
 {
+    /// <summary>
+    /// A delegate to configure the <see cref="ClientWebSocketOptions"/> of the connection.
+    /// </summary>
     public Action<ClientWebSocketOptions>? ConfigureConnectionOptions { get; init; }
 }
