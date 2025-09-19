86d00419c3835aebc133ff6d86e1beef9703c21d
KubaZ2 <jazloty@o2.pl>
2025-09-19 15:22:39 +0200

    Add xml docs for search results

--- NetCord/Rest/GuildMessageSearchResult.cs
+++ NetCord/Rest/GuildMessageSearchResult.cs
@@ -10,7 +10,14 @@ public abstract class GuildMessageSearchResult
 
     public sealed class Success(RestMessage message, GuildMessagesSearchResultData data) : GuildMessageSearchResult
     {
+        /// <summary>
+        /// The message that was found.
+        /// </summary>
         public RestMessage Message => message;
+
+        /// <summary>
+        /// The data associated with the search result.
+        /// </summary>
         public GuildMessagesSearchResultData Data => data;
     }
 
@@ -18,9 +25,24 @@ public abstract class GuildMessageSearchResult
     {
         JsonGuildMessagesSearchResult IJsonModel<JsonGuildMessagesSearchResult>.JsonModel => jsonModel;
 
+        /// <summary>
+        /// The message of the response.
+        /// </summary>
         public string Message => jsonModel.Message!;
+
+        /// <summary>
+        /// The code of the response.
+        /// </summary>
         public int Code => jsonModel.Code.GetValueOrDefault();
+
+        /// <summary>
+        /// The number of documents indexed so far.
+        /// </summary>
         public int DocumentsIndexed => jsonModel.DocumentsIndexed.GetValueOrDefault();
+
+        /// <summary>
+        /// The number of seconds to wait before retrying the request.
+        /// </summary>
         public int RetryAfter => jsonModel.RetryAfter.GetValueOrDefault();
     }
 }
@@ -29,13 +51,28 @@ public class GuildMessagesSearchResultData(JsonGuildMessagesSearchResult jsonMod
 {
     JsonGuildMessagesSearchResult IJsonModel<JsonGuildMessagesSearchResult>.JsonModel => jsonModel;
 
+    /// <summary>
+    /// The analytics ID of the search.
+    /// </summary>
     public string AnalyticsId => jsonModel.AnalyticsId!;
 
+    /// <summary>
+    /// Whether the server is doing a deep historical index.
+    /// </summary>
     public bool DoingDeepHistoricalIndex => jsonModel.DoingDeepHistoricalIndex.GetValueOrDefault();
 
+    /// <summary>
+    /// The total number of results for the search.
+    /// </summary>
     public int TotalResults => jsonModel.TotalResults.GetValueOrDefault();
 
+    /// <summary>
+    /// The threads associated with the search results.
+    /// </summary>
     public IReadOnlyList<GuildThread> Threads { get; } = jsonModel.Threads.SelectOrEmpty(t => GuildThread.CreateFromJson(t, client)).ToArray();
 
+    /// <summary>
+    /// The thread users associated with the search results.
+    /// </summary>
     public IReadOnlyList<ThreadUser> ThreadUsers { get; } = jsonModel.ThreadUsers.SelectOrEmpty(t => new ThreadUser(t, client)).ToArray();
 }
