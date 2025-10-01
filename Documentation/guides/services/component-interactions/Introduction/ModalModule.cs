13c369ad180208b0fa2f4d735765d1965a5d3bf4
Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
2025-10-01 10:45:28 +0200

    Update a receiving modal guide example (#176)

--- Documentation/guides/services/component-interactions/Introduction/ModalModule.cs
+++ Documentation/guides/services/component-interactions/Introduction/ModalModule.cs
@@ -6,6 +6,8 @@ namespace MyBot;
 public class ModalModule : ComponentInteractionModule<ModalInteractionContext>
 {
     [ComponentInteraction("modal")]
-    public string Modal() => string.Join('\n', Context.Components.OfType<TextInput>()
+    public string Modal() => string.Join('\n', Context.Components.OfType<Label>()
+                                                                 .Select(l => l.Component)
+                                                                 .OfType<TextInput>()
                                                                  .Select(i => $"{i.CustomId}: {i.Value}"));
 }
