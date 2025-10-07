f3435291c65f3e4a706bb44d31073440567f315e
KubaZ2 <jazloty@o2.pl>
2025-10-07 23:03:24 +0200

    Add sub command tests

--- Tests/ServicesTest/CommandServiceTests.cs
+++ Tests/ServicesTest/CommandServiceTests.cs
@@ -77,4 +77,86 @@ public class CommandServiceTests : ServiceTests<CommandServiceTester>
             "test 1 test",
             ResultHandler.DataMatch(1)).ConfigureAwait(false);
     }
+
+    [TestMethod]
+    public async ValueTask SubCommands()
+    {
+        var session = _tester.StartSession();
+
+        session.AddCommandGroup("group", builder =>
+        {
+            var currentBuilder = builder;
+            for (int i = 0; i < 100; i++)
+                currentBuilder = currentBuilder.AddSubCommandGroup([$"group{i}"]);
+
+            currentBuilder.AddSubCommand(["test1"], () => Body.Data(1));
+        });
+
+        session.AddCommandGroup("group", builder =>
+        {
+            var currentBuilder = builder;
+            for (int i = 0; i < 100; i++)
+                currentBuilder = currentBuilder.AddSubCommandGroup([$"group{i}"]);
+
+            currentBuilder.AddSubCommand(["test2"], () => Body.Data(2));
+        });
+
+        await session.ExecuteAsync(string.Join(' ', Enumerable.Range(0, 100).Select(i => $"group{i}").Prepend("group").Append("test1")),
+                                   ResultHandler.DataMatch(1)).ConfigureAwait(false);
+
+        await session.ExecuteAsync(string.Join(' ', Enumerable.Range(0, 100).Select(i => $"group{i}").Prepend("group").Append("test2")),
+                                   ResultHandler.DataMatch(2)).ConfigureAwait(false);
+    }
+
+    [TestMethod]
+    public async ValueTask SubCommandsWithParameters()
+    {
+        var session = _tester.StartSession();
+
+        session.AddCommandGroup("group", builder =>
+        {
+            var currentBuilder = builder;
+            for (int i = 0; i < 100; i++)
+                currentBuilder = currentBuilder.AddSubCommandGroup([$"group{i}"]);
+
+            currentBuilder.AddSubCommand(["test"], (string s) => Body.Data(s));
+        });
+
+        await session.ExecuteAsync(string.Join(' ', Enumerable.Range(0, 100).Select(i => $"group{i}").Prepend("group").Append("test").Append("testvalue")),
+                                   ResultHandler.DataMatch("testvalue")).ConfigureAwait(false);
+    }
+
+    [TestMethod]
+    public async ValueTask SubCommandsPriority()
+    {
+        var session = _tester.StartSession();
+
+        session.AddCommandGroup("group", builder =>
+        {
+            builder.AddSubCommand(["test"], (string s) => Body.Data(s));
+
+            builder.AddSubCommandGroup(["test"], builder =>
+            {
+                builder.AddSubCommand(["xd"], () => Body.Data(0));
+            });
+        });
+
+        await session.ExecuteAsync("group test xd", ResultHandler.DataMatch(0)).ConfigureAwait(false);
+
+        await session.ExecuteAsync("group test test", ResultHandler.DataMatch("test")).ConfigureAwait(false);
+
+        session.AddCommandGroup("other-group", builder =>
+        {
+            builder.AddSubCommand(["test"], (string s) => Body.Data(s)).WithPriority(1);
+
+            builder.AddSubCommandGroup(["test"], builder =>
+            {
+                builder.AddSubCommand(["xd"], () => Body.Data(0));
+            });
+        });
+
+        await session.ExecuteAsync("other-group test xd", ResultHandler.DataMatch("xd")).ConfigureAwait(false);
+
+        await session.ExecuteAsync("other-group test test", ResultHandler.DataMatch("test")).ConfigureAwait(false);
+    }
 }
