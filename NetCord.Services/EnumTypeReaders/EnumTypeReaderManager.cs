ae4cbc314a8d688195c81d8a9b0d5fca3700c19f
KubaZ2 <jazloty@o2.pl>
2025-10-27 22:27:28 +0100

    Optimize enum type readers

--- NetCord.Services/EnumTypeReaders/EnumTypeReaderManager.cs
+++ NetCord.Services/EnumTypeReaders/EnumTypeReaderManager.cs
@@ -1,24 +1,16 @@
-﻿namespace NetCord.Services.EnumTypeReaders;
+﻿using System.Collections.Concurrent;
 
-internal unsafe class EnumTypeReaderManager<TTypeReader, TKey, TParameter, TConfiguration>(delegate*<TParameter, TKey> getKey, Func<TKey, TParameter, TConfiguration, TTypeReader> factory) where TTypeReader : IEnumTypeReader where TKey : notnull
+namespace NetCord.Services.EnumTypeReaders;
+
+internal unsafe class EnumTypeReaderManager<TTypeReader, TKey, TParameter, TConfiguration>(delegate*<TParameter, TKey> getKey, Func<TKey, TParameter, TConfiguration, TTypeReader> factory)
+    where TTypeReader : IEnumTypeReader where TKey : notnull
 {
-    private readonly Dictionary<TKey, TTypeReader> _typeReaders = [];
+    private readonly ConcurrentDictionary<TKey, TTypeReader> _typeReaders = [];
 
     public TTypeReader GetTypeReader(TParameter parameter, TConfiguration configuration)
     {
-        var typeReaders = _typeReaders;
-        var key = getKey(parameter);
-
-        TTypeReader? enumInfo;
-        lock (typeReaders)
-        {
-            if (!typeReaders.TryGetValue(key, out enumInfo))
-            {
-                enumInfo = factory(key, parameter, configuration);
-                typeReaders.Add(key, enumInfo);
-            }
-        }
-
-        return enumInfo;
+        return _typeReaders.GetOrAdd(getKey(parameter),
+                                     static (key, arg) => arg.Factory(key, arg.Parameter, arg.Configuration),
+                                     (Factory: factory, Parameter: parameter, Configuration: configuration));
     }
 }
