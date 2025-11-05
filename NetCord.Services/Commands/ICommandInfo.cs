72c25da97d7f3d8a0a25f17119a6b324b941f055
KubaZ2 <jazloty@o2.pl>
2025-11-06 00:48:21 +0100

    Reformat `ICommandInfo`

--- NetCord.Services/Commands/ICommandInfo.cs
+++ NetCord.Services/Commands/ICommandInfo.cs
@@ -3,8 +3,11 @@
 public interface ICommandInfo<TContext> where TContext : ICommandContext
 {
     public IReadOnlyList<string> Aliases { get; }
+
     public int Priority { get; }
+
     public IReadOnlyDictionary<Type, IReadOnlyList<Attribute>> Attributes { get; }
+
     public IReadOnlyList<PreconditionAttribute<TContext>> Preconditions { get; }
 
     public ValueTask<CommandExecutionResult> InvokeAsync(ReadOnlyMemory<char> arguments, TContext context, CommandServiceConfiguration<TContext> configuration, IServiceProvider? serviceProvider);
