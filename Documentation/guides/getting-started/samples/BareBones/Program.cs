316f49b392997d1571a9072a3a983430999cfb1b
Mewyk <public@mewyk.com>
2025-12-10 06:24:03 -0800

    Removing older files that were not intended to be here

--- Documentation/guides/getting-started/samples/BareBones/Program.cs
+++
@@ -1,9 +0,0 @@
-using NetCord.Gateway;
-
-// TODO: Complete sample code for bare bones approach
-var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN")!;
-
-var client = new GatewayClient(token);
-
-await client.StartAsync();
-await Task.Delay(-1);
