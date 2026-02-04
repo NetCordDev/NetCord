933210fe6fbbd02a1b4e3e3d992beec5d92c17a3
KubaZ2 <jazloty@o2.pl>
2026-02-04 18:45:46 +0100

    Cleanup

--- NetCord/Gateway/Voice/VoiceClient.DaveSession.cs
+++ NetCord/Gateway/Voice/VoiceClient.DaveSession.cs
@@ -1,6 +1,5 @@
 ﻿using System.Buffers;
 using System.Collections.Concurrent;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
