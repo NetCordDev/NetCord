f8c22a283c7947062f5521867c91682c76eb0877
KubaZ2 <jazloty@o2.pl>
2026-02-08 00:46:57 +0100

    Refactor P/Invoke for XChaCha20Poly1305 methods
    Simplify interop signatures by removing SuppressGCTransition and UnmanagedCallConv attributes. Introduce DllName constant for "libsodium" to avoid hardcoding. No functional changes to method signatures.

--- NetCord/Gateway/Voice/Encryption/XChaCha20Poly1305.cs
+++ NetCord/Gateway/Voice/Encryption/XChaCha20Poly1305.cs
@@ -4,16 +4,14 @@ namespace NetCord.Gateway.Voice.Encryption;
 
 internal static partial class XChaCha20Poly1305
 {
+    private const string DllName = "libsodium";
+
     public const int ABytes = 16;
     public const int NPubBytes = 24;
 
-    [SuppressGCTransition]
-    [LibraryImport("libsodium", EntryPoint = "crypto_aead_xchacha20poly1305_ietf_encrypt")]
-    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    [LibraryImport(DllName, EntryPoint = "crypto_aead_xchacha20poly1305_ietf_encrypt")]
     internal static partial int CryptoAeadXChaCha20Poly1305IetfEncrypt(ref byte c, ref ulong clen_p, ref byte m, ulong mlen, ref byte ad, ulong adlen, ref byte nsec, ref byte npub, ref byte k);
 
-    [SuppressGCTransition]
-    [LibraryImport("libsodium", EntryPoint = "crypto_aead_xchacha20poly1305_ietf_decrypt")]
-    [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
+    [LibraryImport(DllName, EntryPoint = "crypto_aead_xchacha20poly1305_ietf_decrypt")]
     internal static partial int CryptoAeadXChaCha20Poly1305IetfDecrypt(ref byte m, ref ulong mlen_p, ref byte nsec, ref byte c, ulong clen, ref byte ad, ulong adlen, ref byte npub, ref byte k);
 }
