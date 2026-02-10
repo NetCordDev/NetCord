77d8076205b6ec231d283c0a60c8aa4b326c7ee6
KubaZ2 <jazloty@o2.pl>
2026-02-10 22:45:45 +0100

    Refactor: remove null-forgiving operators in ComponentSection
    Removed use of null-forgiving operators when accessing jsonModel.Accessory and jsonModel.Components, passing them directly instead. Also simplified the Select call for Components by using a method group.

--- NetCord/Components/ComponentSection.cs
+++ NetCord/Components/ComponentSection.cs
@@ -7,6 +7,6 @@ public class ComponentSection(JsonComponentSectionComponent jsonModel) : IMessag
     JsonComponentSectionComponent IJsonModel<JsonComponentSectionComponent>.JsonModel => jsonModel;
 
     public int Id => jsonModel.Id;
-    public IComponentSectionAccessoryComponent Accessory { get; } = IComponentSectionAccessoryComponent.CreateFromJson(jsonModel.Accessory!);
-    public IReadOnlyList<IComponentSectionComponent> Components { get; } = jsonModel.Components!.Select(c => IComponentSectionComponent.CreateFromJson(c)).ToArray();
+    public IComponentSectionAccessoryComponent Accessory { get; } = IComponentSectionAccessoryComponent.CreateFromJson(jsonModel.Accessory);
+    public IReadOnlyList<IComponentSectionComponent> Components { get; } = jsonModel.Components.Select(IComponentSectionComponent.CreateFromJson).ToArray();
 }
