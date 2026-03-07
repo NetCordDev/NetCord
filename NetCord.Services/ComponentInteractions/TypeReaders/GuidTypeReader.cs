d034165def326b5154df736ea11134da239d0fad
Mitchell van Manen <ma.van.manen@live.nl>
2026-03-07 12:13:04 +0100

    Update NetCord.Services/ComponentInteractions/TypeReaders/GuidTypeReader.cs
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>

--- NetCord.Services/ComponentInteractions/TypeReaders/GuidTypeReader.cs
+++ NetCord.Services/ComponentInteractions/TypeReaders/GuidTypeReader.cs
@@ -2,5 +2,5 @@
 
 public class GuidTypeReader<TContext> : ComponentInteractionTypeReader<TContext> where TContext : IComponentInteractionContext
 {
-    public override ValueTask<ComponentInteractionTypeReaderResult> ReadAsync(ReadOnlyMemory<char> input, TContext context, ComponentInteractionParameter<TContext> parameter, ComponentInteractionServiceConfiguration<TContext> configuration, IServiceProvider? serviceProvider) => new (Guid.TryParse(input.ToString(), out var value) ? ComponentInteractionTypeReaderResult.Success(value) : ComponentInteractionTypeReaderResult.ParseFail(parameter.Name));
+    public override ValueTask<ComponentInteractionTypeReaderResult> ReadAsync(ReadOnlyMemory<char> input, TContext context, ComponentInteractionParameter<TContext> parameter, ComponentInteractionServiceConfiguration<TContext> configuration, IServiceProvider? serviceProvider) => new(Guid.TryParse(input.Span, out var value) ? ComponentInteractionTypeReaderResult.Success(value) : ComponentInteractionTypeReaderResult.ParseFail(parameter.Name));
 }
