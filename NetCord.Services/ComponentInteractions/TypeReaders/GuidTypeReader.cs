7f6628a3b19793e054b54fc0b41502a0a90e50b2
Mitchell van Manen <ma.van.manen@live.nl>
2026-03-10 19:42:19 +0100

    Add GuidTypeReader for component interaction parameters (#295)
    * Add GuidTypeReader for component interaction
    
    * Include GuidTypeReader in type readers dictionary
    
    Added GuidTypeReader to ComponentInteractionServiceConfiguration.
    
    * Update NetCord.Services/ComponentInteractions/TypeReaders/GuidTypeReader.cs
    
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
    
    * Update NetCord.Services/ComponentInteractions/ComponentInteractionServiceConfiguration.cs
    
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>

---
+++ NetCord.Services/ComponentInteractions/TypeReaders/GuidTypeReader.cs
@@ -0,0 +1,6 @@
+﻿namespace NetCord.Services.ComponentInteractions.TypeReaders;
+
+public class GuidTypeReader<TContext> : ComponentInteractionTypeReader<TContext> where TContext : IComponentInteractionContext
+{
+    public override ValueTask<ComponentInteractionTypeReaderResult> ReadAsync(ReadOnlyMemory<char> input, TContext context, ComponentInteractionParameter<TContext> parameter, ComponentInteractionServiceConfiguration<TContext> configuration, IServiceProvider? serviceProvider) => new(Guid.TryParse(input.Span, out var value) ? ComponentInteractionTypeReaderResult.Success(value) : ComponentInteractionTypeReaderResult.ParseFail(parameter.Name));
+}
