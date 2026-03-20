1b0730cd6dddcb9a94622c66b6e9ff759425257a
KubaZ2 <jazloty@o2.pl>
2026-03-21 00:34:10 +0100

    Add Nix dev for documentation

--- flake.nix
+++ flake.nix
@@ -18,21 +18,36 @@
   in
   {
     devShells = forAllArch (arch:
-        let
-          pkgs = nixpkgs.legacyPackages.${arch};
+      let
+        pkgs = nixpkgs.legacyPackages.${arch};
 
-          globalJson = builtins.fromJSON (builtins.readFile ./global.json);
-          version = globalJson.sdk.version;
-          majorVersion = builtins.head (builtins.splitVersion version);
-          dotnet = pkgs."dotnet-sdk_${majorVersion}";
-        in
-        {
-            default = pkgs.mkShell {
-              packages = [
-                dotnet
-              ];
-            };
-        }
+        globalJson = builtins.fromJSON (builtins.readFile ./global.json);
+        version = globalJson.sdk.version;
+        majorVersion = builtins.head (builtins.splitVersion version);
+        dotnet = pkgs."dotnet-sdk_${majorVersion}";
+
+        docfx = pkgs.buildDotnetGlobalTool {
+          pname = "docfx";
+          version = "2.78.3";
+          nugetHash = "sha256-hLb6OmxqXOOxFaq/N+aZ0sAzEYjU0giX3c1SWQtKDbs=";
+          dotnet-sdk = dotnet;
+        };
+      in
+      {
+        default = pkgs.mkShell {
+          packages = [
+            dotnet
+          ];
+        };
+
+        docs = pkgs.mkShell {
+          packages = [
+            docfx
+            dotnet
+            pkgs.nodejs_25
+          ];
+        };
+      }
     );
   };
 }
