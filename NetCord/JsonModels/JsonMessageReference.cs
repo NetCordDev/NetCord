f5e262e5edf8ccba0f6a7b7cbf69934133d68191
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2026-04-07 14:50:48 -0500

    Add support for `Type` property in MessageReference

--- NetCord/JsonModels/JsonMessageReference.cs
+++ NetCord/JsonModels/JsonMessageReference.cs
@@ -4,6 +4,9 @@ namespace NetCord.JsonModels;
 
 public class JsonMessageReference
 {
+    [JsonPropertyName("type")]
+    public MessageReferenceType? Type { get; set; }
+    
     [JsonPropertyName("message_id")]
     public ulong? MessageId { get; set; }
 
--- NetCord/MessageReference.cs
+++ NetCord/MessageReference.cs
@@ -4,6 +4,7 @@ public class MessageReference(JsonModels.JsonMessageReference jsonModel) : IJson
 {
     JsonModels.JsonMessageReference IJsonModel<JsonModels.JsonMessageReference>.JsonModel => jsonModel;
 
+    public MessageReferenceType? Type => jsonModel.Type;
     public ulong MessageId => jsonModel.MessageId.GetValueOrDefault();
     public ulong ChannelId => jsonModel.ChannelId.GetValueOrDefault();
     public ulong? GuildId => jsonModel.GuildId;
