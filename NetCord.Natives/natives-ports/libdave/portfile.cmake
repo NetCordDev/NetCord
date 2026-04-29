df31759176988977236369a99949fea9268cee24
Haves Irfan <20160532+ha-ves@users.noreply.github.com>
2026-04-30 00:38:00 +0900

    * Add NetCord.Natives project and vcpkg support * Add native ports for libdave and mlspp * Update build workflows for native packaging * Add local targets and CI helpers for natives * Update README

--- NetCord.Natives/natives-ports/libdave/portfile.cmake
+++ NetCord.Natives/natives-ports/libdave/portfile.cmake
@@ -20,10 +20,8 @@ vcpkg_cmake_build()
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-# file(INSTALL
-#     "${SOURCE_PATH}/LICENSE"
-#     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-#     RENAME LICENSE.txt
-# )
+file(INSTALL
+    "${SOURCE_PATH}/LICENSE"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright
+)
