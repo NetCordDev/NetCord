95da1b33b5084a41b2f895dd398a6e65564799b0
Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
2026-06-19 23:19:54 +0200

    Update Opus wrapper (#371)
    * Add new Opus Applications
    
    * Update xml docs to include 80 and 100 ms frame sizes

--- NetCord/Gateway/Voice/Streams/OpusEncodeStreamConfiguration.cs
+++ NetCord/Gateway/Voice/Streams/OpusEncodeStreamConfiguration.cs
@@ -3,7 +3,7 @@ namespace NetCord.Gateway.Voice;
 public class OpusEncodeStreamConfiguration
 {
     /// <summary>
-    /// The duration of each Opus frame, in milliseconds. Defaults to <see cref="Opus.DefaultFrameDuration"/>. Allowed values are 2.5, 5, 10, 20, 40, 60 and 120 (the last one is only allowed for <see cref="VoiceChannels.Mono"/>).
+    /// The duration of each Opus frame, in milliseconds. Defaults to <see cref="Opus.DefaultFrameDuration"/>. Allowed values are 2.5, 5, 10, 20, 40, 60, 80, 100 and 120 (the last one is only allowed for <see cref="VoiceChannels.Mono"/>).
     /// </summary>
     public float? FrameDuration { get; set; }
 
