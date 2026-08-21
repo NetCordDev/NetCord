c47987a5bb18ce26210916339970c9cc109be19c
Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
2026-08-21 16:54:08 +0200

    Improve disconnection logs and disconnect event (#402)
    * Improve disconnection logs and disconnect event
    
    * Make disconnected logs consistent
    
    * Improve DisconnectEventArgs
    
    * Cleanup
    
    * Fix CloseStatus xml doc

--- NetCord/Gateway/WebSocketClient.cs
+++ NetCord/Gateway/WebSocketClient.cs
@@ -335,21 +335,28 @@ public abstract partial class WebSocketClient : IDisposable
         {
             var connection = connectionState.Connection;
 
+            var status = connection.CloseStatus;
+
             var description = connection.CloseStatusDescription;
 
-            Log(LogLevel.Information, description, null, static (s, e) =>
+            Log(LogLevel.Information, (Status: status, Description: description), null, static (s, e) =>
             {
                 return s switch
                 {
-                    null or { Length: 0 } => "Disconnected.",
-                    [.., '.'] => $"Disconnected: {s}",
-                    _ => $"Disconnected: {s}.",
+                    (Status: null, Description: null or { Length: 0 }) => "Disconnected. Close status was not provided. Close status description was not provided.",
+                    (Status: null, Description: [.., '.'] description) => $"Disconnected. Close status was not provided. Close status description: {description}",
+                    (Status: null, Description: { } description) => $"Disconnected. Close status was not provided. Close status description: {description}.",
+                    (Status: { } status, Description: null or { Length: 0 }) => $"Disconnected. Close status: {status}. Close status description was not provided.",
+                    (Status: { } status, Description: [.., '.'] description) => $"Disconnected. Close status: {status}. Close status description: {description}",
+                    (Status: { } status, Description: { } description) => $"Disconnected. Close status: {status}. Close status description: {description}.",
                 };
             });
 
-            var reconnect = Reconnect((WebSocketCloseStatus?)connection.CloseStatus, description);
+            var reconnect = Reconnect(status, description);
 
-            var disconnectTask = InvokeEventAsync(_disconnect, reconnect, static reconnect => new(reconnect));
+            var disconnectTask = InvokeEventAsync(_disconnect,
+                                                  (Status: status, Reconnect: reconnect, Description: description),
+                                                  static data => new((WebSocketCloseStatus?)data.Status, data.Description, data.Reconnect));
 
             if (reconnect)
             {
@@ -787,7 +794,7 @@ public abstract partial class WebSocketClient : IDisposable
         }
     }
 
-    private protected abstract bool Reconnect(WebSocketCloseStatus? status, string? description);
+    private protected abstract bool Reconnect(int? status, string? description);
 
     private protected async ValueTask ResumeAsync(State state)
     {
