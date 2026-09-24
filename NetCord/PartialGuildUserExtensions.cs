90c4e9229682f87be6194f0a3ee259cf3415ac90
Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
2026-09-25 01:32:00 +0200

    Improve PartialGuildUserExtensions.GetRoles to ignore missing roles instead of throwing (#426)
    * Improve PartialGuildUserExtensions.GetRoles to ignore missing roles instead of throwing
    
    * Improve xml docs
    
    * Optimize GetRoles

--- NetCord/PartialGuildUserExtensions.cs
+++ NetCord/PartialGuildUserExtensions.cs
@@ -11,12 +11,24 @@ public static class PartialGuildUserExtensions
     /// <summary>
     /// Returns an <see cref="IEnumerable{Role}"/> object belonging to the <see cref="PartialGuildUser"/> by acquiring it from the specificied <see cref="RestGuild"/>.
     /// </summary>
+    /// <remarks>
+    /// IDs of roles that do not exist in the <see cref="RestGuild"/> will be ignored.
+    /// </remarks>
     /// <param name="user">The <see cref="PartialGuildUser"/> to acquire roles for.</param>
     /// <param name="guild">The <see cref="RestGuild"/> to acquire the roles from.</param>
     public static IEnumerable<Role> GetRoles(this PartialGuildUser user, RestGuild guild)
     {
         var roles = guild.Roles;
-        return user.RoleIds.Select(r => roles[r]);
+
+        var roleIds = user.RoleIds;
+
+        int count = roleIds.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (roles.TryGetValue(roleIds[i], out var role))
+                yield return role;
+        }
     }
 
     /// <summary>
