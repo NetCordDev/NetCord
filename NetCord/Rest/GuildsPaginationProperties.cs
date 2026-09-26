9d191c09dc5913beba874ccf1465f9911d59948e
KubaZ2 <jazloty@o2.pl>
2026-09-26 18:08:22 +0200

    Reorder properties of GuildsPaginationProperties

--- NetCord/Rest/GuildsPaginationProperties.cs
+++ NetCord/Rest/GuildsPaginationProperties.cs
@@ -3,9 +3,9 @@ namespace NetCord.Rest;
 [GenerateMethodsForProperties]
 public partial record GuildsPaginationProperties : PaginationProperties<ulong>, IPaginationProperties<ulong, GuildsPaginationProperties>
 {
-    public bool WithCounts { get; set; }
-
     public int? Shard { get; set; }
+
+    public bool WithCounts { get; set; }
 
     static GuildsPaginationProperties IPaginationProperties<ulong, GuildsPaginationProperties>.Create() => new();
     static GuildsPaginationProperties IPaginationProperties<ulong, GuildsPaginationProperties>.Create(GuildsPaginationProperties properties) => new(properties);
